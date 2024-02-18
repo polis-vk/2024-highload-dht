41641ee48f9793ebefe586f16a6cf006b5b2bd9d
yulalenk <yulya.alenkova@mail.ru>
2024-02-18 21:58:08 +0300

    base implementation of server

---
+++ src/main/java/ru/vk/itmo/test/alenkovayulya/ServiceImpl.java
@@ -0,0 +1,45 @@
+package ru.vk.itmo.test.alenkovayulya;
+
+import ru.vk.itmo.Service;
+import ru.vk.itmo.ServiceConfig;
+import ru.vk.itmo.dao.Config;
+import ru.vk.itmo.test.ServiceFactory;
+import ru.vk.itmo.test.alenkovayulya.dao.ReferenceDao;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+public class ServiceImpl implements Service {
+
+    private ReferenceDao referenceDao;
+    private ServerImpl server;
+    private final ServiceConfig config;
+
+    public ServiceImpl(ServiceConfig config) {
+        this.config = config;
+
+    }
+
+    @Override
+    public CompletableFuture<Void> start() throws IOException {
+        referenceDao = new ReferenceDao(new Config(config.workingDir(), 1024));
+        server = new ServerImpl(config, referenceDao);
+        server.start();
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @Override
+    public CompletableFuture<Void> stop() throws IOException {
+        server.stop();
+        referenceDao.close();
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @ServiceFactory(stage = 1)
+    public static class Factory implements ServiceFactory.Factory {
+        @Override
+        public Service create(ServiceConfig config) {
+            return new ServiceImpl(config);
+        }
+    }
+}

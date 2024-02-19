4e01c45ac61e9b9e10b7c55dfc2e110c29703221
Степан <s.l.shishigin@neovoxtech.ru>
2024-02-20 00:11:08 +0300

    lua random read write delete script

---
+++ src/main/java/ru/vk/itmo/test/shishiginstepan/lua_scripts/random_rw_requests.lua
@@ -0,0 +1,33 @@
+base_url = "/v0/entity?"
+headers = {}
+headers["Host"] = "localhost:8080"
+wrk.port=8080
+function build_query_param(param, value)
+    return param.."="..value
+end
+
+function getOneRandom()
+    return wrk.format("GET", base_url..build_query_param("id", math.random(0, 10000)), headers)
+end
+
+
+function putOneRandom()
+    return wrk.format("PUT", base_url..build_query_param("id", math.random(0, 10000)), headers, math.random(0, 10000))
+end
+
+
+function deleteOneRandom()
+    return wrk.format("DELETE", base_url..build_query_param("id", math.random(0, 10000)), headers)
+end
+
+
+request = function()
+    magic_selector = math.random(0,3)
+    if magic_selector < 2 then
+        return getOneRandom()
+    elseif magic_selector == 3 then
+        return putOneRandom()
+    else
+        return deleteOneRandom()
+    end
+end

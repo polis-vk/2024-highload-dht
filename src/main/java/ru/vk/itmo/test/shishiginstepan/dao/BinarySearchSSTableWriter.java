294e3c76833dde3c8c21a71c1bb6c1b720882eaf
Степан <s.l.shishigin@neovoxtech.ru>
2024-02-19 23:59:44 +0300

    dao init

---
+++ src/main/java/ru/vk/itmo/test/shishiginstepan/dao/BinarySearchSSTableWriter.java
@@ -0,0 +1,123 @@
+package ru.vk.itmo.test.shishiginstepan.dao;
+
+import ru.vk.itmo.dao.Entry;
+
+import java.io.IOException;
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class BinarySearchSSTableWriter {
+    private static final ThreadLocal<Path> sstPath = new ThreadLocal<>();
+    private static final ThreadLocal<Path> tempSSTPath = new ThreadLocal<>();
+    private static final ThreadLocal<Path> indexPath = new ThreadLocal<>();
+    private static final ThreadLocal<Path> tempIndexPath = new ThreadLocal<>();
+
+
+    private static class SSTableCreationException extends RuntimeException {
+        public SSTableCreationException(Throwable cause) {
+            super(cause);
+        }
+    }
+
+    public static BinarySearchSSTable writeSSTable(
+            Iterable<Entry<MemorySegment>> entries,
+            Path path,
+            int id,
+            Arena arena
+    ) {
+        sstPath.set(Path.of(path.toAbsolutePath() + "/sstable_" + id));
+        tempSSTPath.set(Path.of(
+                path.toAbsolutePath() + "/tmp-sstable" + ThreadLocalRandom.current().nextInt(0, 1_000_000)
+        ));
+        indexPath.set(Path.of(path.toAbsolutePath() + "/sstable_" + id + "_index"));
+        tempIndexPath.set(Path.of(
+                path.toAbsolutePath() + "/tmp-index" + ThreadLocalRandom.current().nextInt(0, 1_000_000)
+        ));
+        MemorySegment tableSegment;
+        MemorySegment indexSegment;
+        long dataSize = 0;
+        long indexSize = 0;
+        for (var entry : entries) {
+            dataSize += entry.key().byteSize();
+
+            if (entry.value() != null) {
+                dataSize += entry.value().byteSize();
+            }
+            indexSize += Long.BYTES * 2;
+        }
+        try (var fileChannel = FileChannel.open(
+                tempSSTPath.get(),
+                StandardOpenOption.READ,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.TRUNCATE_EXISTING
+        )) {
+            tableSegment = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, dataSize, arena);
+        } catch (IOException e) {
+            throw new SSTableCreationException(e);
+        }
+
+        try (var fileChannel = FileChannel.open(
+                tempIndexPath.get(),
+                StandardOpenOption.READ,
+                StandardOpenOption.CREATE,
+                StandardOpenOption.WRITE,
+                StandardOpenOption.TRUNCATE_EXISTING
+        )) {
+            indexSegment = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, indexSize, arena);
+        } catch (IOException e) {
+            throw new SSTableCreationException(e);
+        }
+        writeEntries(entries, tableSegment, indexSegment);
+        try {
+            Files.move(tempIndexPath.get(), indexPath.get(), StandardCopyOption.ATOMIC_MOVE);
+        } catch (IOException e) {
+            throw new SSTableCreationException(e);
+        }
+
+        try {
+            Files.move(tempSSTPath.get(), sstPath.get(), StandardCopyOption.ATOMIC_MOVE);
+        } catch (IOException e) {
+            throw new SSTableCreationException(e);
+        }
+        return new BinarySearchSSTable(sstPath.get(), arena);
+    }
+
+    private static void writeEntries(
+            Iterable<Entry<MemorySegment>> entries,
+            MemorySegment tableSegment,
+            MemorySegment indexSegment
+    ) {
+        long tableOffset = 0;
+        long indexOffset = 0;
+        for (var entry : entries) {
+            indexSegment.set(ValueLayout.JAVA_LONG_UNALIGNED, indexOffset, tableOffset);
+            indexOffset += ValueLayout.JAVA_LONG_UNALIGNED.byteSize();
+
+            MemorySegment.copy(entry.key(), 0, tableSegment, tableOffset, entry.key().byteSize());
+            tableOffset += entry.key().byteSize();
+            if (entry.value() == null) {
+                indexSegment.set(ValueLayout.JAVA_LONG_UNALIGNED, indexOffset, tombstone(tableOffset));
+                indexOffset += ValueLayout.JAVA_LONG_UNALIGNED.byteSize();
+                continue;
+            }
+            indexSegment.set(ValueLayout.JAVA_LONG_UNALIGNED, indexOffset, tableOffset);
+            indexOffset += ValueLayout.JAVA_LONG_UNALIGNED.byteSize();
+
+            MemorySegment.copy(entry.value(), 0, tableSegment, tableOffset, entry.value().byteSize());
+            tableOffset += entry.value().byteSize();
+        }
+    }
+
+    private static long tombstone(long offset) {
+        return 1L << 63 | offset;
+    }
+
+}
---
+++ src/main/java/ru/vk/itmo/test/shishiginstepan/dao/PersistentStorage.java
@@ -0,0 +1,147 @@
+package ru.vk.itmo.test.shishiginstepan.dao;
+
+import ru.vk.itmo.dao.Entry;
+
+import java.io.IOException;
+import java.lang.foreign.Arena;
+import java.lang.foreign.MemorySegment;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class PersistentStorage {
+    private final Path basePath;
+    private final NavigableSet<BinarySearchSSTable> sstables = new ConcurrentSkipListSet<>(
+            Comparator.comparingInt(o -> -o.id)
+    );
+    private final AtomicInteger lastSSTableId;
+
+    private final Arena arena;
+
+    private static final ThreadLocal<List<BinarySearchSSTable>> tablesToCompact = new ThreadLocal<>();
+
+    private static final class CompactionError extends RuntimeException {
+        public CompactionError(Exception e) {
+            super(e);
+        }
+    }
+
+    PersistentStorage(Path basePath) {
+        arena = Arena.ofShared();
+        this.basePath = basePath;
+        try (var sstablesFiles = Files.list(basePath)) {
+            sstablesFiles.filter(
+                    x -> !x.getFileName().toString().contains("_index") && !x.getFileName().toString().contains("tmp")
+            ).map(
+                    path -> new BinarySearchSSTable(path, arena)).forEach(this.sstables::add);
+        } catch (IOException e) {
+            Logger.getAnonymousLogger().log(Level.WARNING, "Failed reading SSTABLE (probably deleted)");
+        }
+
+        lastSSTableId = new AtomicInteger(this.sstables.isEmpty() ? 0 : this.sstables.getFirst().id);
+    }
+
+    public synchronized int nextId() {
+        return this.lastSSTableId.incrementAndGet();
+    }
+
+    public void close() {
+        arena.close();
+    }
+
+    /**
+     * Гарантирует что при успешном завершении записи на диск, SSTable с переданными в метод данными
+     * сразу будет доступен для чтения в PersistentStorage.
+     **/
+    public void store(Iterable<Entry<MemorySegment>> data, int id) {
+        BinarySearchSSTable newSSTable = BinarySearchSSTableWriter.writeSSTable(
+                data,
+                basePath,
+                id,
+                arena
+        );
+        this.sstables.add(newSSTable);
+    }
+
+    public Entry<MemorySegment> get(MemorySegment key) {
+        for (BinarySearchSSTable sstable : this.sstables) {
+            if (sstable.closed.get()) continue;
+            Entry<MemorySegment> ssTableResult = sstable.get(key);
+            if (ssTableResult != null) {
+                return ssTableResult;
+            }
+        }
+        return null;
+    }
+
+    public void enrichWithPersistentIterators(
+            MemorySegment from,
+            MemorySegment to,
+            List<Iterator<Entry<MemorySegment>>> iteratorsToEnrich
+    ) {
+        iteratorsToEnrich.addAll(getPersistentIterators(from, to));
+    }
+
+    private List<Iterator<Entry<MemorySegment>>> getPersistentIterators(
+            MemorySegment from,
+            MemorySegment to
+    ) {
+        List<Iterator<Entry<MemorySegment>>> iterators = new ArrayList<>(sstables.size() + 1);
+        for (var sstable : sstables) {
+            if (sstable.closed.get()) continue;
+            iterators.add(sstable.scan(from, to));
+        }
+        return iterators;
+    }
+
+    private void setTablesToCompact() {
+        for (var sstable : sstables) {
+            if (sstable.closed.get()) continue;
+            if (sstable.inCompaction.compareAndSet(false, true)) tablesToCompact.get().add(sstable);
+        }
+    }
+
+    private List<Iterator<Entry<MemorySegment>>> getCompactableIterators() {
+        List<Iterator<Entry<MemorySegment>>> iteratorsToCompact = new ArrayList<>();
+        for (var sstable : tablesToCompact.get()) {
+            if (sstable.closed.get()) continue;
+            if (sstable.inCompaction.compareAndSet(false, true)) tablesToCompact.get().add(sstable);
+        }
+
+        for (var sstable : tablesToCompact.get()) {
+            iteratorsToCompact.add(sstable.scan(null, null));
+        }
+        return iteratorsToCompact;
+    }
+
+
+    public void compact(int id) {
+        tablesToCompact.set(new ArrayList<>());
+        setTablesToCompact();
+        store(
+                () -> {
+                    var iterators = getCompactableIterators();
+                    return new SkipDeletedIterator(
+                            new MergeIterator(
+                                    iterators));
+                }, id);
+        for (var sstable : tablesToCompact.get()) {
+            compactionClean(sstable);
+        }
+        tablesToCompact.remove();
+    }
+
+    private void compactionClean(BinarySearchSSTable sstable) {
+        sstable.close();
+        try {
+            Files.delete(sstable.indexPath);
+            Files.delete(sstable.tablePath);
+        } catch (IOException e) {
+            throw new CompactionError(e);
+        }
+    }
+}

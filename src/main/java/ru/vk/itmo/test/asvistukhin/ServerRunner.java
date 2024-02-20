150539b07d439e71ab7d30f57c4ccd2f6598901b
Andrey Svistukhin <a.svistukhin@just-ai.com>
2024-02-21 00:26:29 +0300

    [lab-1] results

--- src/main/java/ru/vk/itmo/test/asvistukhin/ServerRunner.java
+++ src/main/java/ru/vk/itmo/test/asvistukhin/ServerRunner.java
@@ -9,7 +9,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-public class ServerRunner {
+public final class ServerRunner {
 
     private ServerRunner() {
         // hello, codeclimate

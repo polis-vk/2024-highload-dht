d742016c79af85726085c4417ab375380df51fb5
dmitrij <dmitrij.typuev@gmail.com>
2024-02-20 12:29:29 +0300

    added my DAO and http rest api

---
+++ src/main/java/ru/vk/itmo/test/tyapuevdmitrij/ServerStarter.java
@@ -0,0 +1,17 @@
+package ru.vk.itmo.test.tyapuevdmitrij;
+
+import ru.vk.itmo.ServiceConfig;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class ServerStarter {
+    public static void main(String[] args) throws IOException {
+        ServerImplementation server = new ServerImplementation(new ServiceConfig(8080,
+                "http://localhost",
+                List.of("http://localhost"),
+                Files.createTempDirectory(".")));
+        server.start();
+    }
+}

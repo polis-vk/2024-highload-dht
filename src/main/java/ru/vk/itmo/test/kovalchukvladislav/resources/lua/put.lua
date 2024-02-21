427064d089eaf3661ce1b7353067074b5da2efcf
Vladislav Kovalchuk <vladislav.kovalchuk@vk.team>
2024-02-21 23:51:00 +0300

    Add async

--- src/main/java/ru/vk/itmo/test/kovalchukvladislav/resources/lua/put.lua
+++ src/main/java/ru/vk/itmo/test/kovalchukvladislav/resources/lua/put.lua
@@ -5,6 +5,6 @@ headers["Host"] = "localhost:8080"
 request = function()
     counter = counter + 1
     path = "/v0/entity?id=k" .. counter
-    wrk.body = "v" .. counter
-    return wrk.format("PUT", path, headers)
+    body = "v" .. counter
+    return wrk.format("PUT", path, headers, body)
 end

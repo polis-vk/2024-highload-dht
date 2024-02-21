674bbd0a4fdfbe4c69e0458ff998b41abb6f450c
Eugene Kachmar <kachmar521@mail.ru>
2024-02-21 21:57:45 +0300

    more experiments

---
+++ src/main/java/ru/vk/itmo/test/kachmareugene/ServerActivator.java
@@ -0,0 +1,19 @@
+package ru.vk.itmo.test.kachmareugene;
+
+import ru.vk.itmo.ServiceConfig;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class ServerActivator {
+    public static void main(String[] args) throws IOException {
+
+        HttpServerImpl server = new HttpServerImpl(new ServiceConfig(
+                8080, "http://localhost/",
+                List.of("http://localhost/"),
+                Files.createTempDirectory(".")
+        ));
+        server.start();
+    }
+}

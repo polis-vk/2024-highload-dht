6019d1b07f49cf2905b8b5dd73e9cc08f921b171
holeyko <mr.holeyko@gmail.com>
2024-02-22 16:17:18 +0300

    Added utils

--- src/main/java/ru/vk/itmo/test/ryabovvadim/utils/MemorySegmentUtils.java
+++ src/main/java/ru/vk/itmo/test/ryabovvadim/utils/MemorySegmentUtils.java
@@ -1,64 +1,22 @@
 package ru.vk.itmo.test.ryabovvadim.utils;
 
 import java.lang.foreign.MemorySegment;
-
-import static java.lang.foreign.ValueLayout.JAVA_BYTE;
+import java.nio.charset.StandardCharsets;
 
 public final class MemorySegmentUtils {
 
-    public static int compareMemorySegments(MemorySegment left, MemorySegment right) {
-        return compareMemorySegments(
-                left, 0, left == null ? 0 : left.byteSize(),
-                right, 0, right == null ? 0 : right.byteSize()
-        );
+    public static MemorySegment fromString(String value) {
+        if (value == null) {
+            return null;
+        }
+        return MemorySegment.ofArray(value.getBytes(StandardCharsets.UTF_8));
     }
 
-    public static int compareMemorySegments(
-            MemorySegment left,
-            long leftFromOffset,
-            long leftToOffset,
-            MemorySegment right,
-            long rightFromOffset,
-            long rightToOffset
-    ) {
-        if (left == null) {
-            return right == null ? 0 : -1;
-        } else if (right == null) {
-            return 1;
+    public static MemorySegment fromBytes(byte[] bytes) {
+        if (bytes == null) {
+            return null;
         }
-
-        long leftSize = leftToOffset - leftFromOffset;
-        long rightSize = rightToOffset - rightFromOffset;
-        long mismatch = MemorySegment.mismatch(
-                left, leftFromOffset, leftToOffset,
-                right, rightFromOffset, rightToOffset
-        );
-
-        if (mismatch == leftSize) {
-            return -1;
-        }
-        if (mismatch == rightSize) {
-            return 1;
-        }
-        if (mismatch == -1) {
-            return 0;
-        }
-
-        return Byte.compareUnsigned(
-                left.get(JAVA_BYTE, leftFromOffset + mismatch),
-                right.get(JAVA_BYTE, rightFromOffset + mismatch)
-        );
-    }
-
-    public static void copyByteArray(byte[] src, MemorySegment dst, long offsetDst) {
-        MemorySegment.copy(
-                src,
-                0,
-                dst,
-                JAVA_BYTE,
-                offsetDst,
-                src.length
-        );
+        return MemorySegment.ofArray(bytes);
     }
 
     private MemorySegmentUtils() {

18a1acbfb9d888ed9079147fa21070ad3e30524a
Ksenia Solnyshko <frezzerrro@gmail.com>
2024-02-25 23:19:30 +0300

    Add async server

--- src/main/java/ru/vk/itmo/test/solnyshkoksenia/MyHttpServer.java
+++ src/main/java/ru/vk/itmo/test/solnyshkoksenia/MyHttpServer.java
@@ -20,9 +20,16 @@ import java.io.UncheckedIOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class MyHttpServer extends HttpServer {
     private final DaoImpl dao;
+    private static final int PROCESSORS = Runtime.getRuntime().availableProcessors();
+    private final ExecutorService executorService = new ThreadPoolExecutor(PROCESSORS, Integer.MAX_VALUE,
+            5, TimeUnit.SECONDS, new LinkedBlockingQueue<>(), this::reject);
 
     public MyHttpServer(ServiceConfig config) throws IOException {
         super(createHttpServerConfig(config));
@@ -44,64 +51,101 @@ public class MyHttpServer extends HttpServer {
         return new Config(config.workingDir(), Math.round(0.33 * 128 * 1024 * 1024)); // 0.33 * 128mb
     }
 
-    @Override
-    public void handleDefault(Request request, HttpSession session) throws IOException {
-        if (request.getMethod() == Request.METHOD_GET) {
-            session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
+    private void reject(Runnable runnable, ExecutorService executorService) {
+        HttpSession session = ((Task) runnable).session;
+        try {
+            session.sendResponse(new Response(Response.PAYMENT_REQUIRED, Response.EMPTY));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
-        session.sendResponse(new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY));
+    }
+
+    @Override
+    public void handleDefault(Request request, HttpSession session) {
+        executorService.execute(new Task(() -> {
+            try {
+                if (request.getMethod() == Request.METHOD_GET ||
+                        request.getMethod() == Request.METHOD_PUT ||
+                        request.getMethod() == Request.METHOD_DELETE) {
+                    session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
+                    return;
+                }
+                session.sendResponse(new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY));
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }, session));
     }
 
     @Override
     public synchronized void stop() {
+        super.stop();
+        executorService.close();
         try {
             dao.close();
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
-        super.stop();
     }
 
     @Path("/v0/entity")
     @RequestMethod(Request.METHOD_GET)
     public void get(final HttpSession session,
-                    @Param(value = "id", required = true) String id) throws IOException {
-        if (sendResponseIfEmpty(id, session)) {
-            return;
-        }
+                    @Param(value = "id", required = true) String id) {
+        executorService.execute(new Task(() -> {
+            try {
+                if (sendResponseIfEmpty(id, session)) {
+                    return;
+                }
 
-        Entry<MemorySegment> entry = dao.get(toMS(id));
-        if (entry == null) {
-            session.sendResponse(new Response(Response.NOT_FOUND, Response.EMPTY));
-            return;
-        }
-        session.sendResponse(Response.ok(entry.value().toArray(ValueLayout.JAVA_BYTE)));
+                Entry<MemorySegment> entry = dao.get(toMS(id));
+                if (entry == null) {
+                    session.sendResponse(new Response(Response.NOT_FOUND, Response.EMPTY));
+                    return;
+                }
+                session.sendResponse(Response.ok(entry.value().toArray(ValueLayout.JAVA_BYTE)));
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }, session));
     }
 
     @Path("/v0/entity")
     @RequestMethod(Request.METHOD_PUT)
     public void put(final Request request, final HttpSession session,
-                    @Param(value = "id", required = true) String id) throws IOException {
-        if (sendResponseIfEmpty(id, session)) {
-            return;
-        }
-        dao.upsert(new BaseEntry<>(toMS(id), MemorySegment.ofArray(request.getBody())));
-        session.sendResponse(new Response(Response.CREATED, Response.EMPTY));
+                    @Param(value = "id", required = true) String id) {
+        executorService.execute(new Task(() -> {
+            try {
+                if (sendResponseIfEmpty(id, session)) {
+                    return;
+                }
+                dao.upsert(new BaseEntry<>(toMS(id), MemorySegment.ofArray(request.getBody())));
+                session.sendResponse(new Response(Response.CREATED, Response.EMPTY));
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }, session));
     }
 
     @Path("/v0/entity")
     @RequestMethod(Request.METHOD_DELETE)
     public void delete(final HttpSession session,
-                       @Param(value = "id", required = true) String id) throws IOException {
-        if (sendResponseIfEmpty(id, session)) {
-            return;
-        }
-        dao.upsert(new BaseEntry<>(toMS(id), null));
-        session.sendResponse(new Response(Response.ACCEPTED, Response.EMPTY));
+                       @Param(value = "id", required = true) String id) {
+        executorService.execute(new Task(() -> {
+            try {
+                if (sendResponseIfEmpty(id, session)) {
+                    return;
+                }
+                dao.upsert(new BaseEntry<>(toMS(id), null));
+                session.sendResponse(new Response(Response.ACCEPTED, Response.EMPTY));
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }, session));
     }
 
     private boolean sendResponseIfEmpty(String input, final HttpSession session) throws IOException {
-        if (input.isEmpty()) {
+        if (input.isBlank()) {
             session.sendResponse(new Response(Response.BAD_REQUEST, Response.EMPTY));
             return true;
         }
@@ -110,5 +154,20 @@ public class MyHttpServer extends HttpServer {
 
     private MemorySegment toMS(String input) {
         return MemorySegment.ofArray(input.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public static class Task implements Runnable {
+        private final Runnable runnable;
+        private final HttpSession session;
+
+        public Task(Runnable runnable, HttpSession session) {
+            this.runnable = runnable;
+            this.session = session;
+        }
+
+        @Override
+        public void run() {
+            runnable.run();
+        }
     }
 }

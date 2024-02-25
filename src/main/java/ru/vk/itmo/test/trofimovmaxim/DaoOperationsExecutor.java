142bb723c42eab3bd9fb0059c8c8961142fe9107
Trofimov Maksim <maks.trofimov.0303@mail.ru>
2024-02-25 17:42:22 +0300

    fix codeclimate

--- src/main/java/ru/vk/itmo/test/trofimovmaxim/DaoOperationsExecutor.java
+++ src/main/java/ru/vk/itmo/test/trofimovmaxim/DaoOperationsExecutor.java
@@ -1,6 +1,11 @@
 package ru.vk.itmo.test.trofimovmaxim;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class DaoOperationsExecutor {
     private static final RejectedExecutionHandler ABORT_POLICY = new ThreadPoolExecutor.AbortPolicy();
--- src/main/java/ru/vk/itmo/test/trofimovmaxim/ExecutorServiceSettings.java
+++ src/main/java/ru/vk/itmo/test/trofimovmaxim/ExecutorServiceSettings.java
@@ -5,4 +5,7 @@ public class ExecutorServiceSettings {
     static final int MAX_POOL_SIZE = Runtime.getRuntime().availableProcessors();
     static final long TIMEOUT_MS = 200;
     static final int QUEUE_SIZE = MAX_POOL_SIZE * 10;
+
+    private ExecutorServiceSettings() {
+    }
 }

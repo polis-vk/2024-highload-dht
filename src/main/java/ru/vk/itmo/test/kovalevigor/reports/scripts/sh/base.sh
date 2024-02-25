958ffc976799244dccf3596c59d7011b065b8088
IsqK <plabolchar23@gmail.com>
2024-02-26 02:16:35 +0300

    refactor reports

--- src/main/java/ru/vk/itmo/test/kovalevigor/reports/scripts/sh/base.sh
+++ src/main/java/ru/vk/itmo/test/kovalevigor/reports/scripts/sh/base.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 NAME=$(date +"%s")
 JFR="../../info-$NAME.jfr"
-(wrk2 -c 1 -t 1 -L -d $1 -R $2 -s "../lua/$3.lua" http://localhost:8080 > "../../html/${NAME}_wrk.txt") &
-./ap/bin/asprof -e cpu,alloc -d $1 -f $JFR MainServer
+PREFIX="../../html/stage2"
+(wrk2 -c 64 -t 1 -L -d $1 -R $2 -s "../lua/$3.lua" http://localhost:8080 > "${PREFIX}/${NAME}_wrk.txt") &
+./ap/bin/asprof -t -e cpu,alloc,lock -d $1 -f $JFR MainServer
 
 wait
-java -cp ./ap/lib/converter.jar jfr2flame $JFR > "../../html/${NAME}_cpu.html"
-java -cp ./ap/lib/converter.jar jfr2flame --alloc $JFR > "../../html/${NAME}_alloc.html"
+java -cp ./ap/lib/converter.jar jfr2flame --threads $JFR > "${PREFIX}/${NAME}_cpu.html"
+java -cp ./ap/lib/converter.jar jfr2flame --threads --alloc $JFR > "${PREFIX}/${NAME}_alloc.html"
+java -cp ./ap/lib/converter.jar jfr2flame --threads --lock $JFR > "${PREFIX}/${NAME}_lock.html"
 rm $JFR

f8e994b3c343480a2a4ad9cfe5b62e57d9855dac
IsqK <plabolchar23@gmail.com>
2024-02-25 23:30:53 +0300

    hw2

--- src/main/java/ru/vk/itmo/test/kovalevigor/server/Server.java
+++ src/main/java/ru/vk/itmo/test/kovalevigor/server/Server.java
@@ -1,11 +1,9 @@
 package ru.vk.itmo.test.kovalevigor.server;
 
+import one.nio.http.HttpException;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpSession;
-import one.nio.http.Param;
-import one.nio.http.Path;
 import one.nio.http.Request;
-import one.nio.http.RequestMethod;
 import one.nio.http.Response;
 import ru.vk.itmo.dao.BaseEntry;
 import ru.vk.itmo.dao.Config;
@@ -14,29 +12,41 @@ import ru.vk.itmo.dao.Entry;
 import ru.vk.itmo.test.kovalevigor.config.DaoServerConfig;
 import ru.vk.itmo.test.kovalevigor.dao.DaoImpl;
 
-import java.io.Closeable;
 import java.io.IOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 import static one.nio.http.Request.METHOD_DELETE;
 import static one.nio.http.Request.METHOD_GET;
 import static one.nio.http.Request.METHOD_PUT;
 
-public class Server extends HttpServer implements Closeable {
+public class Server extends HttpServer implements AutoCloseable, RejectedExecutionHandler {
 
     private static final String ENTITY = "/v0/entity";
     private static final Charset CHARSET = StandardCharsets.UTF_8;
     private final Dao<MemorySegment, Entry<MemorySegment>> dao;
+    private final ExecutorService executorService;
+    private final BlockingQueue<Runnable> blockingQueue;
+    private static final Logger log = Logger.getAnonymousLogger();
 
     private enum Responses {
         NOT_FOUND(Response.NOT_FOUND),
         CREATED(Response.CREATED),
         ACCEPTED(Response.ACCEPTED),
         BAD_REQUEST(Response.BAD_REQUEST),
-        NOT_ALLOWED(Response.METHOD_NOT_ALLOWED);
+        NOT_ALLOWED(Response.METHOD_NOT_ALLOWED),
+        INTERNAL_ERROR(Response.INTERNAL_ERROR),
+        SERVICE_UNAVAILABLE(Response.SERVICE_UNAVAILABLE);
 
         private final String responseCode;
 
@@ -49,9 +59,75 @@ public class Server extends HttpServer implements Closeable {
         }
     }
 
+    private class Task implements Runnable {
+        public final Request request;
+        public final HttpSession session;
+
+        private Task(Request request, HttpSession session) {
+            this.request = request;
+            this.session = session;
+        }
+
+        @Override
+        public void run() {
+            try {
+                this.handleRequest(request, session);
+            } catch (IOException ioException) {
+                session.handleException(ioException);
+            } catch (Exception exception) {
+                Server.Responses response = Responses.INTERNAL_ERROR;
+                if (exception instanceof HttpException) {
+                    response = Responses.BAD_REQUEST;
+                } else {
+                    log.log(Level.SEVERE, exception.getMessage());
+                }
+                sendResponseWithoutIo(session, response);
+            }
+        }
+
+        private void handleRequest(Request request, HttpSession session) throws IOException {
+            if (request.getPath().equals(ENTITY)) {
+                switch (request.getMethod()) {
+                    case METHOD_GET:
+                    case METHOD_PUT:
+                    case METHOD_DELETE:
+                        String entityId = request.getParameter("id=");
+                        if (entityId == null || entityId.isEmpty()) {
+                            break;
+                        }
+                        MemorySegment key = fromString(entityId);
+                        session.sendResponse(
+                                switch (request.getMethod()) {
+                                    case METHOD_GET -> getEntity(key);
+                                    case METHOD_PUT -> createEntity(key, MemorySegment.ofArray(request.getBody()));
+                                    case METHOD_DELETE -> deleteEntity(key);
+                                    default -> throw new IllegalStateException("Can't be");
+                                }
+                        );
+                        return;
+                    default:
+                        session.sendResponse(Responses.NOT_ALLOWED.toResponse());
+                        return;
+                }
+            }
+            session.sendResponse(Responses.BAD_REQUEST.toResponse());
+        }
+    }
+
     public Server(DaoServerConfig config) throws IOException {
         super(config);
         dao = new DaoImpl(mapConfig(config));
+        blockingQueue = new ArrayBlockingQueue<>(200);
+        executorService =  new ThreadPoolExecutor(2, 20,
+                100L, TimeUnit.MILLISECONDS,
+                blockingQueue,
+                this
+                );
+    }
+
+    @Override
+    public void handleRequest(Request request, HttpSession session) {
+        executorService.execute(new Task(request, session));
     }
 
     @Override
@@ -59,53 +135,40 @@ public class Server extends HttpServer implements Closeable {
         session.sendResponse(Responses.BAD_REQUEST.toResponse());
     }
 
-    @Path({ENTITY})
-    public void notAllowed(Request request, HttpSession session) throws IOException {
-        session.sendResponse(Responses.NOT_ALLOWED.toResponse());
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+        sendResponseWithoutIo(((Task) r).session, Responses.SERVICE_UNAVAILABLE);
     }
 
-    @Path(ENTITY)
-    @RequestMethod(METHOD_GET)
-    public Response getEntity(@Param(value = "id", required = true) String entityId) {
-        if (entityId.isEmpty()) {
-            return Responses.BAD_REQUEST.toResponse();
+    private static void sendResponseWithoutIo(HttpSession session, Responses response) {
+        try {
+            session.sendResponse(response.toResponse());
+        } catch (IOException ioException) {
+            session.handleException(ioException);
         }
-        Entry<MemorySegment> entity = dao.get(fromString(entityId));
+    }
+
+    private Response getEntity(MemorySegment key) {
+        Entry<MemorySegment> entity = dao.get(key);
         if (entity == null) {
             return Responses.NOT_FOUND.toResponse();
         }
         return Response.ok(entity.value().toArray(ValueLayout.JAVA_BYTE));
     }
 
-    @Path(ENTITY)
-    @RequestMethod(METHOD_PUT)
-    public Response createEntity(
-            @Param(value = "id", required = true) String entityId,
-            Request request
-    ) {
-        if (entityId.isEmpty()) {
-            return Responses.BAD_REQUEST.toResponse();
-        }
-        dao.upsert(
-                makeEntry(fromString(entityId), MemorySegment.ofArray(request.getBody()))
-        );
+    private Response createEntity(MemorySegment key, MemorySegment value) {
+        dao.upsert(makeEntry(key, value));
         return Responses.CREATED.toResponse();
     }
 
-    @Path(ENTITY)
-    @RequestMethod(METHOD_DELETE)
-    public Response deleteEntity(@Param(value = "id", required = true) String entityId) {
-        if (entityId.isEmpty()) {
-            return Responses.BAD_REQUEST.toResponse();
-        }
-        dao.upsert(
-                makeEntry(fromString(entityId), null)
-        );
+    private Response deleteEntity(MemorySegment key) {
+        dao.upsert(makeEntry(key, null));
         return Responses.ACCEPTED.toResponse();
     }
 
     @Override
     public void close() throws IOException {
+        executorService.close();
         dao.close();
     }
 

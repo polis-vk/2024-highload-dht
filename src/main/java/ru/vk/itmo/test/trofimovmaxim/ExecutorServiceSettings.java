0c839812868b0289d9018f5ee331c7953a75a320
Trofimov Maksim <maks.trofimov.0303@mail.ru>
2024-02-25 17:43:20 +0300

    fix codeclimate

--- src/main/java/ru/vk/itmo/test/trofimovmaxim/ExecutorServiceSettings.java
+++ src/main/java/ru/vk/itmo/test/trofimovmaxim/ExecutorServiceSettings.java
@@ -1,6 +1,6 @@
 package ru.vk.itmo.test.trofimovmaxim;
 
-public class ExecutorServiceSettings {
+public final class ExecutorServiceSettings {
     static final int CORE_POOL_SIZE = 1;
     static final int MAX_POOL_SIZE = Runtime.getRuntime().availableProcessors();
     static final long TIMEOUT_MS = 200;

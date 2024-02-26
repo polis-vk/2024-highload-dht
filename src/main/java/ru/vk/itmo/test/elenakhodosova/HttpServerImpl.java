66b385deaf445af0c96f42d242d70afeeca2d62f
Elena Khodosova <evkhodosova@gmail.com>
2024-02-26 17:49:18 +0300

    Stage 1: fix after review

--- src/main/java/ru/vk/itmo/test/elenakhodosova/HttpServerImpl.java
+++ src/main/java/ru/vk/itmo/test/elenakhodosova/HttpServerImpl.java
@@ -12,6 +12,7 @@ import one.nio.server.AcceptorConfig;
 import one.nio.util.Utf8;
 import ru.vk.itmo.ServiceConfig;
 import ru.vk.itmo.dao.BaseEntry;
+import ru.vk.itmo.dao.Dao;
 import ru.vk.itmo.dao.Entry;
 import ru.vk.itmo.test.elenakhodosova.dao.ReferenceDao;
 
@@ -23,7 +24,7 @@ import java.util.concurrent.ExecutorService;
 
 public class HttpServerImpl extends HttpServer {
 
-    private final ReferenceDao dao;
+    private final Dao<MemorySegment, Entry<MemorySegment>> dao;
     private static final String PATH_NAME = "/v0/entity";
     private final ExecutorService executorService;
 
@@ -62,28 +63,42 @@ public class HttpServerImpl extends HttpServer {
     @RequestMethod(Request.METHOD_GET)
     public Response getEntity(@Param(value = "id", required = true) String id) {
         if (isParamIncorrect(id)) return new Response(Response.BAD_REQUEST, Response.EMPTY);
-        Entry<MemorySegment> value = dao.get(MemorySegment.ofArray(Utf8.toBytes(id)));
-        return value == null ? new Response(Response.NOT_FOUND, Response.EMPTY)
-                : Response.ok(value.value().toArray(ValueLayout.JAVA_BYTE));
+        try {
+            Entry<MemorySegment> value = dao.get(MemorySegment.ofArray(Utf8.toBytes(id)));
+            return value == null ? new Response(Response.NOT_FOUND, Response.EMPTY)
+                    : Response.ok(value.value().toArray(ValueLayout.JAVA_BYTE));
+        } catch (Exception ex) {
+            return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+        }
     }
 
     @Path(PATH_NAME)
     @RequestMethod(Request.METHOD_PUT)
     public Response putEntity(@Param(value = "id", required = true) String id, Request request) {
+        if (isParamIncorrect(id) || request.getBody() == null) {
+            return new Response(Response.BAD_REQUEST, Response.EMPTY);
+        }
         byte[] value = request.getBody();
-        if (isParamIncorrect(id)) return new Response(Response.BAD_REQUEST, Response.EMPTY);
-        dao.upsert(new BaseEntry<>(
-                MemorySegment.ofArray(Utf8.toBytes(id)),
-                MemorySegment.ofArray(value)));
-        return new Response(Response.CREATED, Response.EMPTY);
+        try {
+            dao.upsert(new BaseEntry<>(
+                    MemorySegment.ofArray(Utf8.toBytes(id)),
+                    MemorySegment.ofArray(value)));
+            return new Response(Response.CREATED, Response.EMPTY);
+        } catch (Exception ex) {
+            return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+        }
     }
 
     @Path(PATH_NAME)
     @RequestMethod(Request.METHOD_DELETE)
     public Response deleteEntity(@Param(value = "id", required = true) String id) {
         if (isParamIncorrect(id)) return new Response(Response.BAD_REQUEST, Response.EMPTY);
-        dao.upsert(new BaseEntry<>(MemorySegment.ofArray(Utf8.toBytes(id)), null));
-        return new Response(Response.ACCEPTED, Response.EMPTY);
+        try {
+            dao.upsert(new BaseEntry<>(MemorySegment.ofArray(Utf8.toBytes(id)), null));
+            return new Response(Response.ACCEPTED, Response.EMPTY);
+        } catch (Exception ex) {
+            return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+        }
     }
 
     @Path(PATH_NAME)
@@ -95,17 +110,6 @@ public class HttpServerImpl extends HttpServer {
     public void handleDefault(Request request, HttpSession session) throws IOException {
         Response badRequest = new Response(Response.BAD_REQUEST, Response.EMPTY);
         session.sendResponse(badRequest);
-    }
-
-    @Override
-    public synchronized void stop() {
-        executorService.shutdownNow();
-        super.stop();
-        try {
-            dao.close();
-        } catch (IOException e) {
-            Thread.currentThread().interrupt();
-        }
     }
 
     private boolean isParamIncorrect(String param) {

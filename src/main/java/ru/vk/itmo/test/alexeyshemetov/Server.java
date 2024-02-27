e59ebc81f6bc9a62b4732169a1c5a422ffd90be8
alexshemetov <alexshemetov0@gmail.com>
2024-02-27 22:44:59 +0300

    added solution for stage 2

--- src/main/java/ru/vk/itmo/test/alexeyshemetov/Server.java
+++ src/main/java/ru/vk/itmo/test/alexeyshemetov/Server.java
@@ -6,7 +6,6 @@ import one.nio.http.HttpSession;
 import one.nio.http.Param;
 import one.nio.http.Path;
 import one.nio.http.Request;
-import one.nio.http.RequestMethod;
 import one.nio.http.Response;
 import one.nio.server.AcceptorConfig;
 import ru.vk.itmo.ServiceConfig;
@@ -21,11 +20,25 @@ import java.io.UncheckedIOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
+@SuppressWarnings("unused")
 public class Server extends HttpServer {
     private final Dao<MemorySegment, Entry<MemorySegment>> dao;
     private static final String ENTITY_PATH = "/v0/entity";
     private static final long MEM_DAO_SIZE = 1L << 16;
+    private static final int KEEP_ALIVE_THREAD = 30;
+    private static final TimeUnit KEEP_ALIVE_THREAD_UNIT = TimeUnit.SECONDS;
+    private static final int AWAIT_TERMINATION_TIME = 5;
+    private static final TimeUnit AWAIT_TERMINATION_UNIT = TimeUnit.MINUTES;
+    public static final int QUEUE_CAPACITY = 1024;
+    private final ExecutorService executorService;
 
     public Server(ServiceConfig config) throws IOException {
         super(createServerConfig(config));
@@ -34,17 +47,27 @@ public class Server extends HttpServer {
         } catch (IOException e) {
             throw new UncheckedIOException("Can't start server", e);
         }
+        executorService = getExecutorService();
     }
 
     @Path(ENTITY_PATH)
-    @RequestMethod(Request.METHOD_GET)
-    public Response getById(
-        @Param(value = "id", required = true) final String id
+    public Response entityById(
+        @Param(value = "id", required = true) final String id,
+        Request request
     ) {
         if (id.isBlank()) {
             return new Response(Response.BAD_REQUEST, Response.EMPTY);
         }
 
+        return switch (request.getMethod()) {
+            case Request.METHOD_GET -> getById(id);
+            case Request.METHOD_PUT -> putById(id, request);
+            case Request.METHOD_DELETE -> deleteById(id);
+            default -> notAllowed();
+        };
+    }
+
+    public Response getById(final String id) {
         MemorySegment key = toMemorySegment(id);
         Entry<MemorySegment> segmentEntry = dao.get(key);
         if (segmentEntry == null) {
@@ -53,40 +76,20 @@ public class Server extends HttpServer {
         return Response.ok(segmentEntry.value().toArray(ValueLayout.JAVA_BYTE));
     }
 
-    @Path(ENTITY_PATH)
-    @RequestMethod(Request.METHOD_PUT)
-    public Response putById(
-        @Param(value = "id", required = true) final String id,
-        Request request
-    ) {
-        if (id.isBlank()) {
-            return new Response(Response.BAD_REQUEST, Response.EMPTY);
-        }
-
+    public Response putById(final String id, Request request) {
         MemorySegment key = toMemorySegment(id);
         MemorySegment value = MemorySegment.ofArray(request.getBody());
         dao.upsert(new BaseEntry<>(key, value));
         return new Response(Response.CREATED, Response.EMPTY);
     }
 
-    @Path(ENTITY_PATH)
-    @RequestMethod(Request.METHOD_DELETE)
-    public Response deleteById(
-        @Param(value = "id", required = true) final String id
-    ) {
-        if (id.isBlank()) {
-            return new Response(Response.BAD_REQUEST, Response.EMPTY);
-        }
-
+    public Response deleteById(final String id) {
         MemorySegment key = toMemorySegment(id);
         dao.upsert(new BaseEntry<>(key, null));
         return new Response(Response.ACCEPTED, Response.EMPTY);
     }
 
-    @Path(ENTITY_PATH)
-    public Response notAllowed(
-        @Param(value = "id", required = true) final String id
-    ) {
+    public Response notAllowed() {
         return new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
     }
 
@@ -97,13 +100,62 @@ public class Server extends HttpServer {
     }
 
     @Override
+    public void handleRequest(Request request, HttpSession session) throws IOException {
+        try {
+            Future<?> submit = executorService.submit(() -> {
+                try {
+                    handleRequestWrapper(request, session);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            });
+        } catch (RejectedExecutionException e) {
+            Response response = new Response(Response.SERVICE_UNAVAILABLE, Response.EMPTY);
+            session.sendResponse(response);
+        }
+    }
+
+    private void handleRequestWrapper(Request request, HttpSession session) throws IOException {
+        try {
+            super.handleRequest(request, session);
+        } catch (Exception e) {
+            handleDefault(request, session);
+        }
+    }
+
+    @Override
     public synchronized void stop() {
+        shutdownAwait(executorService);
         super.stop();
         try {
             dao.close();
         } catch (IOException e) {
             throw new UncheckedIOException("Error while stopping server", e);
         }
+    }
+
+    private static void shutdownAwait(ExecutorService executorService) {
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(AWAIT_TERMINATION_TIME, AWAIT_TERMINATION_UNIT)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private static ExecutorService getExecutorService() {
+        int cores = Runtime.getRuntime().availableProcessors();
+        return new ThreadPoolExecutor(
+            cores / 2,
+            cores,
+            KEEP_ALIVE_THREAD,
+            KEEP_ALIVE_THREAD_UNIT,
+            new ArrayBlockingQueue<>(QUEUE_CAPACITY),
+            Executors.defaultThreadFactory(),
+            new ThreadPoolExecutor.DiscardOldestPolicy()
+        );
     }
 
     private static MemorySegment toMemorySegment(String string) {

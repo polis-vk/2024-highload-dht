14d6c82edf3f96d724b75407e3c79a23e3d72e41
Eugene Kachmar <kachmar521@mail.ru>
2024-02-28 22:22:06 +0300

    add Executor Service

--- src/main/java/ru/vk/itmo/test/kachmareugene/HttpServerImpl.java
+++ src/main/java/ru/vk/itmo/test/kachmareugene/HttpServerImpl.java
@@ -21,7 +21,11 @@ import java.io.UncheckedIOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class HttpServerImpl extends HttpServer {
 

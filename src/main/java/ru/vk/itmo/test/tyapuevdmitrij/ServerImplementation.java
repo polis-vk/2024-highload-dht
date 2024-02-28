dbf04ffa28ac7e98962a2b3fe8395fe802c0dea7
dmitrij <dmitrij.typuev@gmail.com>
2024-02-28 20:45:50 +0300

    homework №2 done

--- src/main/java/ru/vk/itmo/test/tyapuevdmitrij/ServerImplementation.java
+++ src/main/java/ru/vk/itmo/test/tyapuevdmitrij/ServerImplementation.java
@@ -3,10 +3,7 @@ package ru.vk.itmo.test.tyapuevdmitrij;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
 import one.nio.http.HttpSession;
-import one.nio.http.Param;
-import one.nio.http.Path;
 import one.nio.http.Request;
-import one.nio.http.RequestMethod;
 import one.nio.http.Response;
 import one.nio.server.AcceptorConfig;
 import org.slf4j.Logger;
@@ -20,18 +17,32 @@ import java.io.IOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ServerImplementation extends HttpServer {
-
-    private final MemorySegmentDao memorySegmentDao;
 
     private static final Logger logger = LoggerFactory.getLogger(ServerImplementation.class);
 
     private static final String ENTITY_PATH = "/v0/entity";
 
+    private static final int threadPoolSize = 4;
+
+    private static final int poolKeepAliveSeconds = 10;
+
+    private static final int threadPoolQueueSize = 16;
+
+    private final MemorySegmentDao memorySegmentDao;
+
+    private final ExecutorService executor;
+
     public ServerImplementation(ServiceConfig config, MemorySegmentDao memorySegmentDao) throws IOException {
         super(createServerConfig(config));
         this.memorySegmentDao = memorySegmentDao;
+        executor = new ThreadPoolExecutor(threadPoolSize, threadPoolSize, poolKeepAliveSeconds, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<>(threadPoolQueueSize));
     }
 
     private static HttpServerConfig createServerConfig(ServiceConfig serviceConfig) {
@@ -44,14 +55,7 @@ public class ServerImplementation extends HttpServer {
         return serverConfig;
     }
 
-    @Path("/v0/status")
-    public Response status() {
-        return Response.ok("OK");
-    }
-
-    @Path(ENTITY_PATH)
-    @RequestMethod(Request.METHOD_GET)
-    public Response get(@Param(value = "id") String id) {
+    private Response get(String id) {
         if (id == null || id.isEmpty()) {
             return new Response(Response.BAD_REQUEST, Response.EMPTY);
         }
@@ -64,18 +68,39 @@ public class ServerImplementation extends HttpServer {
     }
 
     @Override
-    public void handleRequest(Request request, HttpSession session) throws IOException {
-        try {
-            super.handleRequest(request, session);
-        } catch (Exception e) {
-            logger.error("Exception in request method :", e);
-            session.sendResponse(new Response(Response.INTERNAL_ERROR, Response.EMPTY));
-        }
+    public synchronized void stop() {
+        super.stop();
+        executor.close();
     }
 
-    @Path(ENTITY_PATH)
-    @RequestMethod(Request.METHOD_PUT)
-    public Response put(@Param(value = "id") String id, Request request) {
+    @Override
+    public void handleRequest(Request request, HttpSession session) throws IOException {
+        executor.execute(() -> {
+            try {
+                if (!request.getPath().equals(ENTITY_PATH)) {
+                    handleDefault(request, session);
+                }
+                int requestMethod = request.getMethod();
+                switch (requestMethod) {
+                    case Request.METHOD_GET -> session.sendResponse(get(request.getParameter("id=")));
+                    case Request.METHOD_PUT -> session.sendResponse(put(request.getParameter("id="), request));
+                    case Request.METHOD_DELETE -> session.sendResponse(delete(request.getParameter("id=")));
+                    default -> session.sendResponse(unsupportedMethods());
+                }
+            } catch (Exception e) {
+                logger.error("Exception in request method :", e);
+                try {
+                    session.sendResponse(new Response(Response.INTERNAL_ERROR, Response.EMPTY));
+                } catch (IOException ex) {
+                    logger.error("can't send response", e);
+                    session.close();
+                }
+            }
+
+        });
+    }
+
+    private Response put(String id, Request request) {
         if (id == null || id.isEmpty() || request.getBody() == null) {
             return new Response(Response.BAD_REQUEST, Response.EMPTY);
         }
@@ -89,9 +114,7 @@ public class ServerImplementation extends HttpServer {
         return new Response(Response.CREATED, Response.EMPTY);
     }
 
-    @Path(ENTITY_PATH)
-    @RequestMethod(Request.METHOD_DELETE)
-    public Response delete(@Param(value = "id") String id) {
+    private Response delete(String id) {
         if (id == null || id.isEmpty()) {
             return new Response(Response.BAD_REQUEST, Response.EMPTY);
         }
@@ -106,8 +129,7 @@ public class ServerImplementation extends HttpServer {
         session.sendResponse(response);
     }
 
-    @Path(ENTITY_PATH)
-    public Response unsupportedMethods() {
+    private Response unsupportedMethods() {
         return new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
     }
 

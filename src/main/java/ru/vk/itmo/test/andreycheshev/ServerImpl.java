25a35c2f2884da4ad1060a273cb7d71f4c84f4df
Andrey <70713475+Queenore@users.noreply.github.com>
2024-02-29 01:51:55 +0300

    code review fixes

--- src/main/java/ru/vk/itmo/test/andreycheshev/ServerImpl.java
+++ src/main/java/ru/vk/itmo/test/andreycheshev/ServerImpl.java
@@ -5,12 +5,12 @@ import one.nio.http.HttpServerConfig;
 import one.nio.http.HttpSession;
 import one.nio.http.Request;
 import one.nio.http.Response;
-import one.nio.server.AcceptorConfig;
-import ru.vk.itmo.ServiceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.vk.itmo.dao.BaseEntry;
 import ru.vk.itmo.dao.Config;
 import ru.vk.itmo.dao.Entry;
-import ru.vk.itmo.test.andreycheshev.dao.ReferenceDao;
+import ru.vk.itmo.test.andreycheshev.dao.PersistentReferenceDao;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
@@ -24,37 +24,25 @@ import static one.nio.http.Request.METHOD_PUT;
 import static one.nio.http.Response.ACCEPTED;
 import static one.nio.http.Response.BAD_REQUEST;
 import static one.nio.http.Response.CREATED;
+import static one.nio.http.Response.INTERNAL_ERROR;
 import static one.nio.http.Response.METHOD_NOT_ALLOWED;
 import static one.nio.http.Response.NOT_FOUND;
 import static one.nio.http.Response.OK;
 
 public class ServerImpl extends HttpServer {
-    private static final int THRESHOLD_BYTES = 100000;
+    private static final Logger logger = LoggerFactory.getLogger(ServerImpl.class);
     private static final String REQUEST_PATH = "/v0/entity";
     private static final String ID = "id=";
-    private final ReferenceDao dao;
 
-    public ServerImpl(ServiceConfig config) throws IOException {
-        super(createServerConfig(config));
+    private final PersistentReferenceDao dao;
 
-        Config daoConfig = new Config(config.workingDir(), THRESHOLD_BYTES);
+    public ServerImpl(HttpServerConfig config, Config daoConfig) throws IOException {
+        super(config);
 
-        this.dao = new ReferenceDao(daoConfig);
+        this.dao = new PersistentReferenceDao(daoConfig);
     }
 
-    public static HttpServerConfig createServerConfig(ServiceConfig config) {
-        AcceptorConfig acceptorConfig = new AcceptorConfig();
-        acceptorConfig.port = config.selfPort();
-        acceptorConfig.reusePort = true;
-
-        HttpServerConfig serverConfig = new HttpServerConfig();
-        serverConfig.acceptors = new AcceptorConfig[]{acceptorConfig};
-        serverConfig.closeSessions = true;
-
-        return serverConfig;
-    }
-
-    public Response get(final Request request) {
+    private Response get(final Request request) {
         String id = request.getParameter(ID);
         if (id == null || id.isEmpty()) {
             return new Response(BAD_REQUEST, Response.EMPTY);
@@ -67,7 +55,7 @@ public class ServerImpl extends HttpServer {
                 : new Response(OK, entry.value().toArray(ValueLayout.JAVA_BYTE));
     }
 
-    public Response put(final Request request) {
+    private Response put(final Request request) {
         String id = request.getParameter(ID);
         if (id == null || id.isEmpty()) {
             return new Response(BAD_REQUEST, Response.EMPTY);
@@ -82,7 +70,7 @@ public class ServerImpl extends HttpServer {
         return new Response(CREATED, Response.EMPTY);
     }
 
-    public Response delete(final Request request) {
+    private Response delete(final Request request) {
         String id = request.getParameter(ID);
         if (id == null || id.isEmpty()) {
             return new Response(BAD_REQUEST, Response.EMPTY);
@@ -95,23 +83,41 @@ public class ServerImpl extends HttpServer {
     }
 
     @Override
-    public void handleRequest(Request request, HttpSession session) throws IOException {
+    public void handleRequest(Request request, HttpSession session) {
         String path = request.getPath();
         if (!path.equals(REQUEST_PATH)) {
-            session.sendResponse(new Response(BAD_REQUEST, Response.EMPTY));
+            sendResponse(
+                    new Response(BAD_REQUEST, Response.EMPTY),
+                    session
+            );
             return;
         }
 
         int method = request.getMethod();
 
-        Response response = switch (method) {
-            case METHOD_GET -> get(request);
-            case METHOD_PUT -> put(request);
-            case METHOD_DELETE -> delete(request);
-            default -> new Response(METHOD_NOT_ALLOWED, Response.EMPTY);
-        };
+        Response response;
+        try {
+            response = switch (method) {
+                case METHOD_GET -> get(request);
+                case METHOD_PUT -> put(request);
+                case METHOD_DELETE -> delete(request);
+                default -> new Response(METHOD_NOT_ALLOWED, Response.EMPTY);
+            };
+        } catch (Exception e) {
+            logger.error("Internal error of the DAO operation", e);
+            sendResponse(new Response(INTERNAL_ERROR, Response.EMPTY), session);
+            return;
+        }
 
-        session.sendResponse(response);
+        sendResponse(response, session);
+    }
+
+    private void sendResponse(Response response, HttpSession session) {
+        try {
+            session.sendResponse(response);
+        } catch (IOException e) {
+            throw new UncheckedIOException("Error while sending response to the client", e);
+        }
     }
 
     private MemorySegment fromString(String data) {
@@ -120,11 +126,11 @@ public class ServerImpl extends HttpServer {
 
     @Override
     public synchronized void stop() {
+        super.stop();
         try {
             dao.close();
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
-        super.stop();
     }
 }
--- src/main/java/ru/vk/itmo/test/andreycheshev/ServerStarter.java
+++ src/main/java/ru/vk/itmo/test/andreycheshev/ServerStarter.java
@@ -1,24 +1,31 @@
 package ru.vk.itmo.test.andreycheshev;
 
-import ru.vk.itmo.ServiceConfig;
+import one.nio.http.HttpServerConfig;
+import one.nio.server.AcceptorConfig;
+import ru.vk.itmo.dao.Config;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
 
 public final class ServerStarter {
+    private static final Path dirPath = Path.of("/home/andrey/andrey/tmp");
+    private static final int THRESHOLD_BYTES = 100000;
+
     private ServerStarter() {
 
     }
 
     public static void main(String[] args) throws IOException {
-        ServerImpl server = new ServerImpl(
-                new ServiceConfig(
-                        8080,
-                        "http://localhost",
-                        List.of("http://localhost"),
-                        Path.of("/home/andrey/andrey/tmp")
-                ));
+        AcceptorConfig acceptorConfig = new AcceptorConfig();
+        acceptorConfig.port = 8080;
+        acceptorConfig.reusePort = true;
+        HttpServerConfig serverConfig = new HttpServerConfig();
+        serverConfig.acceptors = new AcceptorConfig[]{acceptorConfig};
+        serverConfig.closeSessions = true;
+
+        Config daoConfig = new Config(dirPath, THRESHOLD_BYTES);
+
+        ServerImpl server = new ServerImpl(serverConfig, daoConfig);
 
         server.start();
     }

59530644ad0294bd29f86c2925ae357af3be56a1
osokindm <osokin34@gmail.com>
2024-02-28 20:37:25 +0300

    hw2 first try

--- src/main/java/ru/vk/itmo/test/osokindm/HttpServerImpl.java
+++ src/main/java/ru/vk/itmo/test/osokindm/HttpServerImpl.java
@@ -6,33 +6,141 @@ import one.nio.http.HttpSession;
 import one.nio.http.Param;
 import one.nio.http.Path;
 import one.nio.http.Request;
-import one.nio.http.RequestMethod;
 import one.nio.http.Response;
 import one.nio.server.AcceptorConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.vk.itmo.ServiceConfig;
-import ru.vk.itmo.dao.Config;
 import ru.vk.itmo.dao.Entry;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class HttpServerImpl extends HttpServer {
 
-    private static final int MEMORY_LIMIT = 8388608;
-    private static final String PATH = "/v0/entity";
+    private static final String DEFAULT_PATH = "/v0/entity";
     private static final String ID_REQUEST = "id=";
-    private DaoWrapper daoWrapper;
-    private final java.nio.file.Path workingDir;
+    private static final Logger logger = LoggerFactory.getLogger(HttpServerImpl.class);
+    private final ExecutorService requestWorkers;
+    private final DaoWrapper daoWrapper;
 
-    private final Logger logger = LoggerFactory.getLogger(HttpServerImpl.class);
 
-    public HttpServerImpl(ServiceConfig config) throws IOException {
+    public HttpServerImpl(ServiceConfig config, DaoWrapper daoWrapper) throws IOException {
         super(createServerConfig(config));
-        workingDir = config.workingDir();
-        daoWrapper = new DaoWrapper(new Config(config.workingDir(), MEMORY_LIMIT));
+        this.daoWrapper = daoWrapper;
+        requestWorkers = new ThreadPoolExecutor(64, 64, 60L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(128));
+    }
+
+    @Override
+    public synchronized void stop() {
+        try {
+            daoWrapper.stop();
+            requestWorkers.close();
+        } catch (IOException e) {
+            logger.error("Error occurred while closing database");
+        }
+        super.stop();
+    }
+
+    @Path(DEFAULT_PATH)
+    public Response entity(Request request, @Param(value = "id", required = true) String id) {
+        if (id == null || id.isBlank()) {
+            return new Response(Response.BAD_REQUEST, Response.EMPTY);
+        }
+
+        switch (request.getMethod()) {
+            case Request.METHOD_GET -> {
+                Future<Response> response = requestWorkers.submit(() -> {
+                    Entry<MemorySegment> result = daoWrapper.get(id);
+                    if (result != null && result.value() != null) {
+                        return Response.ok(result.value().toArray(ValueLayout.JAVA_BYTE));
+                    }
+                    return new Response(Response.NOT_FOUND, Response.EMPTY);
+                });
+                try {
+                    return response.get();
+                } catch (InterruptedException e) {
+                    logger.error("GET operation was interrupted");
+                } catch (ExecutionException e) {
+                    logger.error("Error occurred while executing GET");
+                }
+                return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+            }
+
+            case Request.METHOD_PUT -> {
+                Future<Response> response = requestWorkers.submit(() -> {
+                    daoWrapper.upsert(id, request);
+                    return new Response(Response.CREATED, Response.EMPTY);
+                });
+                try {
+                    return response.get();
+                } catch (InterruptedException e) {
+                    logger.error("UPSERT operation was interrupted");
+                } catch (ExecutionException e) {
+                    logger.error("Error occurred while executing UPSERT");
+                }
+                return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+            }
+
+            case Request.METHOD_DELETE -> {
+                Future<Response> response = requestWorkers.submit(() -> {
+                    daoWrapper.delete(id);
+                    return new Response(Response.ACCEPTED, Response.EMPTY);
+                });
+                try {
+                    return response.get();
+                } catch (InterruptedException e) {
+                    logger.error("DELETE operation was interrupted");
+                } catch (ExecutionException e) {
+                    logger.error("Error occurred while executing DELETE");
+                }
+                return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+            }
+
+            default -> {
+                return new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
+            }
+        }
+    }
+
+    @Override
+    public void handleRequest(Request request, HttpSession session) {
+//        requestWorkers.execute(() -> {
+        String id = request.getParameter(ID_REQUEST);
+        if (id != null && id.isEmpty()) {
+            try {
+                handleDefault(request, session);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        } else {
+            try {
+                super.handleRequest(request, session);
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+//        });
+    }
+
+    @Override
+    public void handleDefault(Request request, HttpSession session) throws IOException {
+        Response response = new Response(Response.BAD_REQUEST, Response.EMPTY);
+        try {
+            session.sendResponse(response);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
     }
 
     private static HttpServerConfig createServerConfig(ServiceConfig serviceConfig) {
@@ -45,73 +153,6 @@ public class HttpServerImpl extends HttpServer {
         serverConfig.closeSessions = true;
 
         return serverConfig;
-    }
-
-    @Override
-    public synchronized void start() {
-        try {
-            daoWrapper = new DaoWrapper(new Config(workingDir, MEMORY_LIMIT));
-        } catch (IOException e) {
-            logger.error("Error occurred while creating database");
-        }
-
-        super.start();
-    }
-
-    @Override
-    public synchronized void stop() {
-        try {
-            daoWrapper.stop();
-        } catch (IOException e) {
-            logger.error("Error occurred while closing database");
-        }
-        super.stop();
-    }
-
-    @Path(PATH)
-    @RequestMethod(Request.METHOD_GET)
-    public Response get(@Param(value = ID_REQUEST, required = true) String id) {
-        Entry<MemorySegment> result = daoWrapper.get(id);
-        if (result != null && result.value() != null) {
-            return new Response(Response.OK, result.value().toArray(ValueLayout.JAVA_BYTE));
-        }
-        return new Response(Response.NOT_FOUND, Response.EMPTY);
-    }
-
-    @Path(PATH)
-    @RequestMethod(Request.METHOD_DELETE)
-    public Response delete(@Param(value = ID_REQUEST, required = true) String id) {
-        daoWrapper.delete(id);
-        return new Response(Response.ACCEPTED, Response.EMPTY);
-    }
-
-    @Path(PATH)
-    @RequestMethod(Request.METHOD_PUT)
-    public Response upsert(@Param(value = ID_REQUEST, required = true) String id, Request request) {
-        daoWrapper.upsert(id, request);
-        return new Response(Response.CREATED, Response.EMPTY);
-    }
-
-    @Path(PATH)
-    @RequestMethod(Request.METHOD_POST)
-    public Response post() {
-        return new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
-    }
-
-    @Override
-    public void handleRequest(Request request, HttpSession session) throws IOException {
-        String id = request.getParameter(ID_REQUEST);
-        if (id != null && id.isEmpty()) {
-            handleDefault(request, session);
-        } else {
-            super.handleRequest(request, session);
-        }
-    }
-
-    @Override
-    public void handleDefault(Request request, HttpSession session) throws IOException {
-        Response response = new Response(Response.BAD_REQUEST, Response.EMPTY);
-        session.sendResponse(response);
     }
 
 }
--- src/main/java/ru/vk/itmo/test/osokindm/TestServer.java
+++ src/main/java/ru/vk/itmo/test/osokindm/TestServer.java
@@ -5,6 +5,7 @@ import ru.vk.itmo.ServiceConfig;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public final class TestServer {
 
@@ -12,11 +13,15 @@ public final class TestServer {
     }
 
     public static void main(String[] args) throws IOException {
-        HttpServerImpl server = new HttpServerImpl(new ServiceConfig(
+        ServiceImpl service = new ServiceImpl(new ServiceConfig(
                 8080, "http://localhost",
                 List.of("http://localhost"),
                 Path.of("/home/john/database/")
         ));
-        server.start();
+        try {
+            service.start().get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

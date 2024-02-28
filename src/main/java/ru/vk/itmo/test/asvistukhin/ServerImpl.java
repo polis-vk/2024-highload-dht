9de63bea12c22debc9ffbd6fe8d8e4cdd554ec4a
Andrey Svistukhin <a.svistukhin@just-ai.com>
2024-02-28 18:49:07 +0300

    [lab-2] init

--- src/main/java/ru/vk/itmo/test/asvistukhin/ServerImpl.java
+++ src/main/java/ru/vk/itmo/test/asvistukhin/ServerImpl.java
@@ -1,5 +1,7 @@
 package ru.vk.itmo.test.asvistukhin;
 
+import one.nio.async.CustomThreadFactory;
+import one.nio.http.HttpException;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
 import one.nio.http.HttpSession;
@@ -10,11 +12,38 @@ import ru.vk.itmo.ServiceConfig;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ServerImpl extends HttpServer {
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
+
+    private static final String HTTP_SERVICE_NOT_AVAILABLE = "503";
+    private static final String EMPTY_RESPONSE = "";
+    private static final int QUEUE_CAPACITY = 2000;
+
+    private final ThreadPoolExecutor executor;
 
     public ServerImpl(ServiceConfig config) throws IOException {
         super(createHttpServerConfig(config));
+        executor = new ThreadPoolExecutor(
+            50,
+            50,
+            20,
+            TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(QUEUE_CAPACITY),
+            new CustomThreadFactory("server-executor", false),
+            new ThreadPoolExecutor.AbortPolicy()
+        );
+    }
+
+    @Override
+    public void handleRequest(Request request, HttpSession session) {
+        executor.execute(() -> wrapHandleRequest(request, session));
     }
 
     @Override
@@ -27,6 +56,36 @@ public class ServerImpl extends HttpServer {
             ? new Response(Response.BAD_REQUEST, Response.EMPTY)
             : new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
         session.sendResponse(response);
+    }
+
+    @Override
+    public synchronized void stop() {
+        executor.close();
+        super.stop();
+    }
+
+    private void wrapHandleRequest(Request request, HttpSession session) {
+        try {
+            super.handleRequest(request, session);
+        } catch (RejectedExecutionException executionException) {
+            try {
+                logger.log(Level.SEVERE, "Rejected execution new request.", executionException);
+                session.sendError(HTTP_SERVICE_NOT_AVAILABLE, "Server is overload.");
+            } catch (IOException ioException) {
+                logger.log(Level.SEVERE, "Failed send error response to client.", ioException);
+                session.close();
+                Thread.currentThread().interrupt();
+            }
+        } catch (Exception e) {
+            try {
+                String response = e.getClass() == HttpException.class ? Response.BAD_REQUEST : Response.INTERNAL_ERROR;
+                session.sendError(response, EMPTY_RESPONSE);
+            } catch (IOException ex) {
+                logger.log(Level.SEVERE, "Failed send error response to client.", e);
+                session.close();
+                Thread.currentThread().interrupt();
+            }
+        }
     }
 
     private static HttpServerConfig createHttpServerConfig(ServiceConfig serviceConfig) {

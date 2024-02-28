adef3c10413ba43f5fed7b827e5e324384ee53b4
Andrey <70713475+Queenore@users.noreply.github.com>
2024-02-29 02:48:08 +0300

    style fixes

--- src/main/java/ru/vk/itmo/test/andreycheshev/ServerStarter.java
+++ src/main/java/ru/vk/itmo/test/andreycheshev/ServerStarter.java
@@ -8,8 +8,9 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public final class ServerStarter {
-    private static final Path dirPath = Path.of("/home/andrey/andrey/tmp");
+    private static final Path DIT_PATH = Path.of("/home/andrey/andrey/tmp");
     private static final int THRESHOLD_BYTES = 100000;
+    private static final int PORT = 8080;
 
     private ServerStarter() {
 
@@ -17,13 +18,13 @@ public final class ServerStarter {
 
     public static void main(String[] args) throws IOException {
         AcceptorConfig acceptorConfig = new AcceptorConfig();
-        acceptorConfig.port = 8080;
+        acceptorConfig.port = PORT;
         acceptorConfig.reusePort = true;
         HttpServerConfig serverConfig = new HttpServerConfig();
         serverConfig.acceptors = new AcceptorConfig[]{acceptorConfig};
         serverConfig.closeSessions = true;
 
-        Config daoConfig = new Config(dirPath, THRESHOLD_BYTES);
+        Config daoConfig = new Config(DIT_PATH, THRESHOLD_BYTES);
 
         ServerImpl server = new ServerImpl(serverConfig, daoConfig);
 

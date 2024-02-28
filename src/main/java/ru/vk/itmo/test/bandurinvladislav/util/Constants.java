2318d5bd70d7309dc28640fc9a4642e4081b71d4
vbandurin7 <v.bandurin.w@mail.ru>
2024-02-28 23:43:09 +0300

    codeclimate

--- src/main/java/ru/vk/itmo/test/bandurinvladislav/util/Constants.java
+++ src/main/java/ru/vk/itmo/test/bandurinvladislav/util/Constants.java
@@ -1,6 +1,6 @@
 package ru.vk.itmo.test.bandurinvladislav.util;
 
-public class Constants {
+public final class Constants {
     public static final String ENDPOINT = "/v0/entity";
     public static final String PARAMETER_ID = "id=";
     public static final int FLUSH_THRESHOLD_BYTES = 12 * 1024 * 1024;

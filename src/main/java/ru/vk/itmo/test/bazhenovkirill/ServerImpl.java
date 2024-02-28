74534c44d26ce52a201342f361d14103a7273e76
dasu <kirill12093@mail.ru>
2024-02-28 20:04:57 +0300

    fix: issues from reviewer

--- src/main/java/ru/vk/itmo/test/bazhenovkirill/ServerImpl.java
+++ src/main/java/ru/vk/itmo/test/bazhenovkirill/ServerImpl.java
@@ -22,13 +22,15 @@ import java.nio.charset.StandardCharsets;
 
 public class ServerImpl extends HttpServer {
 
-    private static final String API_PREFIX = "/v0/entity";
+    private static final long FLUSHING_THRESHOLD_BYTES = 1024 * 1024;
+
+    private static final String API_ENDPOINT = "/v0/entity";
 
     private final DaoImpl dao;
 
     public ServerImpl(ServiceConfig config) throws IOException {
         super(createServerConfig(config));
-        dao = new DaoImpl(new Config(config.workingDir(), 1024 * 1024));
+        dao = new DaoImpl(new Config(config.workingDir(), FLUSHING_THRESHOLD_BYTES));
     }
 
     private static HttpServerConfig createServerConfig(ServiceConfig config) {
@@ -42,54 +44,56 @@ public class ServerImpl extends HttpServer {
         return serverConfig;
     }
 
-    @Path(API_PREFIX)
-    @RequestMethod(Request.METHOD_GET)
-    public Response getEntityById(@Param(value = "id", required = true) String id) {
+    @Path(API_ENDPOINT)
+    public Response entity(Request request, @Param(value = "id") String id) {
         if (isInvalidId(id)) {
             return new Response(Response.BAD_REQUEST, Response.EMPTY);
         }
 
-        MemorySegment key = convertStringToMemorySegment(id);
-        Entry<MemorySegment> entry = dao.get(key);
+        switch (request.getMethod()) {
+            case Request.METHOD_GET -> {
+                MemorySegment key = convertStringToMemorySegment(id);
+                Entry<MemorySegment> entry = dao.get(key);
 
-        if (entry == null) {
-            return new Response(Response.NOT_FOUND, Response.EMPTY);
+                if (entry == null) {
+                    return new Response(Response.NOT_FOUND, Response.EMPTY);
+                }
+
+                return Response.ok(entry.value().toArray(ValueLayout.JAVA_BYTE));
+            }
+            case Request.METHOD_DELETE -> {
+                MemorySegment key = convertStringToMemorySegment(id);
+                try {
+                    dao.upsert(new BaseEntry<>(key, null));
+                } catch (IllegalStateException ex) {
+                    return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+                }
+                return new Response(Response.ACCEPTED, Response.EMPTY);
+            }
+            case Request.METHOD_PUT -> {
+                MemorySegment key = convertStringToMemorySegment(id);
+                MemorySegment value = MemorySegment.ofArray(request.getBody());
+                try {
+                    dao.upsert(new BaseEntry<>(key, value));
+                } catch (IllegalStateException ex) {
+                    return new Response(Response.INTERNAL_ERROR, Response.EMPTY);
+                }
+
+                return new Response(Response.CREATED, Response.EMPTY);
+            }
+            default -> {
+                return new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
+            }
         }
-
-        return Response.ok(entry.value().toArray(ValueLayout.JAVA_BYTE));
     }
 
-    @Path(API_PREFIX)
-    @RequestMethod(Request.METHOD_DELETE)
-    public Response deleteEntityById(@Param(value = "id", required = true) String id) {
-        if (isInvalidId(id)) {
-            return new Response(Response.BAD_REQUEST, Response.EMPTY);
+    @Override
+    public void handleRequest(Request request, HttpSession session) throws IOException {
+        try {
+            super.handleRequest(request, session);
+        } catch (IOException ex) {
+            session.sendResponse(new Response(Response.INTERNAL_ERROR, Response.EMPTY));
         }
-
-        MemorySegment key = convertStringToMemorySegment(id);
-        dao.upsert(new BaseEntry<>(key, null));
-
-        return new Response(Response.ACCEPTED, Response.EMPTY);
-    }
-
-    @Path(API_PREFIX)
-    public Response respondMethodNotAllowed() {
-        return new Response(Response.METHOD_NOT_ALLOWED, Response.EMPTY);
-    }
-
-    @Path(API_PREFIX)
-    @RequestMethod(Request.METHOD_PUT)
-    public Response putEntity(@Param(value = "id", required = true) String id, Request request) {
-        byte[] body = request.getBody();
-        if (isInvalidId(id) || body == null) {
-            return new Response(Response.BAD_REQUEST, Response.EMPTY);
-        }
-
-        MemorySegment key = convertStringToMemorySegment(id);
-        MemorySegment value = MemorySegment.ofArray(request.getBody());
-        dao.upsert(new BaseEntry<>(key, value));
-
-        return new Response(Response.CREATED, Response.EMPTY);
     }
 
     @Override
@@ -100,16 +104,16 @@ public class ServerImpl extends HttpServer {
 
     @Override
     public synchronized void stop() {
+        super.stop();
         try {
             dao.close();
         } catch (IOException e) {
             throw new IllegalArgumentException(e);
         }
-        super.stop();
     }
 
     private boolean isInvalidId(String id) {
-        return id == null || id.isEmpty();
+        return id == null || id.isBlank();
     }
 
     private MemorySegment convertStringToMemorySegment(String id) {

83f3164b3fdb4b804985eaccf94e4f0e3f606ab6
lehatheslayer <ziqunion95@gmail.com>
2024-02-29 17:48:28 +0300

    codeclimate #2

--- src/main/java/ru/vk/itmo/test/reshetnikovaleksei/ExecutorServiceFactory.java
+++ src/main/java/ru/vk/itmo/test/reshetnikovaleksei/ExecutorServiceFactory.java
@@ -6,7 +6,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-public class ExecutorServiceFactory {
+public final class ExecutorServiceFactory {
     private static final int CORE_POOL_SIZE = 10;
     private static final int MAXIMUM_POOL_SIZE = 20;
     private static final long KEEP_ALIVE_TIME = 60;

7b425a51ecbf4737fda710c95826e264e11583a1
dasu <kirill12093@mail.ru>
2024-02-29 15:41:23 +0300

    fix: cg

--- src/main/java/ru/vk/itmo/test/bazhenovkirill/ServerImpl.java
+++ src/main/java/ru/vk/itmo/test/bazhenovkirill/ServerImpl.java
@@ -22,7 +22,11 @@ import java.io.UncheckedIOException;
 import java.lang.foreign.MemorySegment;
 import java.lang.foreign.ValueLayout;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ServerImpl extends HttpServer {
 
@@ -145,17 +149,21 @@ public class ServerImpl extends HttpServer {
     public synchronized void stop() {
         super.stop();
         try {
-            executorService.shutdown();
-            try {
-                if (!executorService.awaitTermination(30, TimeUnit.MILLISECONDS)) {
-                    executorService.shutdownNow();
-                }
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
+            shutdownExecutor();
             dao.close();
         } catch (IOException e) {
             throw new IllegalArgumentException(e);
+        }
+    }
+
+    private void shutdownExecutor() {
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(30, TimeUnit.MILLISECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
         }
     }
 

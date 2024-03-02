a6055407e0da9256a1f080be49f2030089e385e6
lollyhunter <sdm.01@mail.ru>
2024-03-02 18:35:04 +0300

    hw2 report

--- src/main/java/ru/vk/itmo/test/smirnovdmitrii/server/DaoWorkerPool.java
+++ src/main/java/ru/vk/itmo/test/smirnovdmitrii/server/DaoWorkerPool.java
@@ -5,8 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;

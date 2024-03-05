7a80ddaf3d3e4eca6254bdc3a83d56ae4afc12f6
llav3ji2019 <pavelemelyanov03@mail.ru>
2024-03-05 06:48:57 +0300

    add report for stage2

--- src/main/java/ru/vk/itmo/test/pavelemelyanov/ExecutorServiceConfig.java
+++ src/main/java/ru/vk/itmo/test/pavelemelyanov/ExecutorServiceConfig.java
@@ -10,7 +10,7 @@ public final class ExecutorServiceConfig {
     public static final int CORE_POOL_SIZE = CORE_AMOUNT / 2;
     public static final int MAX_CORE_POOL_SIZE = CORE_AMOUNT;
     public static final int KEEP_ALIVE_TIME = 200;
-    public static final int QUEUE_CAPACITY = MAX_CORE_POOL_SIZE * 2;
+    public static final int QUEUE_CAPACITY = 64;
     public static final BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
     public static final RejectedExecutionHandler HANDLER = new ThreadPoolExecutor.AbortPolicy();
 

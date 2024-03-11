43ef7fb47d1127b5408b927af2b17aa7ed4d12f4
osokindm <osokin34@gmail.com>
2024-03-11 21:17:30 +0300

    hw 2

--- src/main/java/ru/vk/itmo/test/osokindm/ServiceImpl.java
+++ src/main/java/ru/vk/itmo/test/osokindm/ServiceImpl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.vk.itmo.Service;
 import ru.vk.itmo.ServiceConfig;
+import ru.vk.itmo.dao.Config;
 import ru.vk.itmo.test.ServiceFactory;
 
 import java.io.IOException;
@@ -11,19 +12,21 @@ import java.util.concurrent.CompletableFuture;
 
 public class ServiceImpl implements Service {
 
+    private static final int MEMORY_LIMIT_BYTES = 8 * 1024 * 1024;
+    private static final Logger logger = LoggerFactory.getLogger(HttpServerImpl.class);
     private final ServiceConfig config;
-    private final Logger logger;
+    private DaoWrapper daoWrapper;
     private HttpServerImpl server;
 
     public ServiceImpl(ServiceConfig config) {
         this.config = config;
-        this.logger = LoggerFactory.getLogger(HttpServerImpl.class);
     }
 
     @Override
     public CompletableFuture<Void> start() throws IOException {
         try {
-            server = new HttpServerImpl(config);
+            daoWrapper = new DaoWrapper(new Config(config.workingDir(), MEMORY_LIMIT_BYTES));
+            server = new HttpServerImpl(config, daoWrapper);
             server.start();
         } catch (IOException e) {
             logger.error("Error occurred while starting the server");
@@ -34,9 +37,14 @@ public class ServiceImpl implements Service {
 
     @Override
     public CompletableFuture<Void> stop() {
+        try {
+            daoWrapper.stop();
+        } catch (IOException e) {
+            logger.error("Error occurred while closing database");
+        }
         server.stop();
         return CompletableFuture.completedFuture(null);
-     }
+    }
 
     @ServiceFactory(stage = 1)
     public static class Factory implements ServiceFactory.Factory {

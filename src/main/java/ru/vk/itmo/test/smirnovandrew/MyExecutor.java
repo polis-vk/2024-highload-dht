dcdc8d2c15c1e458bc8a29a0c2fae087a6890f0a
Andrei_Smirnov <sandrew2003@gmail.com>
2024-03-13 16:56:29 +0300

    executor fix

--- src/main/java/ru/vk/itmo/test/smirnovandrew/MyExecutor.java
+++ src/main/java/ru/vk/itmo/test/smirnovandrew/MyExecutor.java
@@ -1,8 +1,6 @@
 package ru.vk.itmo.test.smirnovandrew;
 
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class MyExecutor extends ThreadPoolExecutor {
 
@@ -10,28 +8,61 @@ public class MyExecutor extends ThreadPoolExecutor {
 
     private static final int QUEUE_CAPACITY = 1000;
 
+    private static final int FIRST_TIMEOUT = 2;
+
+    private static final int SECOND_TIMEOUT = 2;
+
+
     public MyExecutor(int corePoolSize, int maximumPoolSize) {
         super(
                 corePoolSize,
                 maximumPoolSize,
                 KEEP_ALIVE_TIME,
                 TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(QUEUE_CAPACITY)
+                new MyStack<>(QUEUE_CAPACITY)
         );
+    }
+
+    private static class MyStack<E> extends LinkedBlockingDeque<E> implements BlockingQueue<E> {
+
+        public MyStack(int capacity) {
+            super(capacity);
+        }
+
+        @Override
+        public void put(E e) throws InterruptedException {
+            super.putFirst(e);
+        }
+
+        @Override
+        public boolean offer(E e) {
+            return super.offerFirst(e);
+        }
+
+        @Override
+        public boolean offer(E e, long timeout, TimeUnit unit) throws InterruptedException {
+            return super.offerFirst(e, timeout, unit);
+        }
+
+        @Override
+        public boolean add(E e) {
+            super.addFirst(e);
+            return true;
+        }
     }
 
     @Override
     public void shutdown() {
         super.shutdown();
         try {
-            super.awaitTermination(2, TimeUnit.SECONDS);
+            super.awaitTermination(FIRST_TIMEOUT, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
 
         super.shutdownNow();
         try {
-            super.awaitTermination(1, TimeUnit.SECONDS);
+            super.awaitTermination(SECOND_TIMEOUT, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }

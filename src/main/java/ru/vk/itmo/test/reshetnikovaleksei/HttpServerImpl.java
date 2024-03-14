4b197fd4ded19163fcc6649bc941dad49acd0183
lehatheslayer <ziqunion95@gmail.com>
2024-03-14 18:17:40 +0300

    style

--- src/main/java/ru/vk/itmo/test/reshetnikovaleksei/HttpServerImpl.java
+++ src/main/java/ru/vk/itmo/test/reshetnikovaleksei/HttpServerImpl.java
@@ -1,6 +1,5 @@
 package ru.vk.itmo.test.reshetnikovaleksei;
 
-import one.nio.http.HttpException;
 import one.nio.http.HttpServer;
 import one.nio.http.HttpServerConfig;
 import one.nio.http.HttpSession;

298d7413e839d7b320bc80be40a185c36fa3a68c
Dmitrii <90109742+smirnovdm2107@users.noreply.github.com>
2024-03-17 21:15:24 +0300

    HW2 Cмирнов Дмитрий Максимович, ИТМО ФИТиП M33361  (#46)
    * dao
    
    * server
    
    * improve 1
    
    * imporove 2
    
    * report 1
    
    * dao
    
    * server
    
    * improve 1
    
    * imporove 2
    
    * report 1
    
    * tmp
    
    * async
    
    * hw2 report
    
    * hw2 report
    
    * properties excluded

--- src/main/java/ru/vk/itmo/test/smirnovdmitrii/server/DaoHttpServerConfig.java
+++ src/main/java/ru/vk/itmo/test/smirnovdmitrii/server/DaoHttpServerConfig.java
@@ -6,4 +6,9 @@ import java.nio.file.Path;
 
 public class DaoHttpServerConfig extends HttpServerConfig {
     public Path workingDir;
+    public boolean useWorkers;
+    public int queueSize;
+    public int keepAliveTime;
+    public boolean useVirtualThreads;
+    public WorkerQueueType workerQueueType;
 }

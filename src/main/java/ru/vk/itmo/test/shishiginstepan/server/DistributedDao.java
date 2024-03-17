87850323cad10cef455458614507e579df890268
Степан <s.l.shishigin@neovoxtech.ru>
2024-03-17 20:39:20 +0300

    change hashing from java message digest to one nio murmur3 implementation

--- src/main/java/ru/vk/itmo/test/shishiginstepan/server/DistributedDao.java
+++ src/main/java/ru/vk/itmo/test/shishiginstepan/server/DistributedDao.java
@@ -1,5 +1,6 @@
 package ru.vk.itmo.test.shishiginstepan.server;
 
+import one.nio.util.Hash;
 import ru.vk.itmo.dao.Dao;
 import ru.vk.itmo.dao.Entry;
 
@@ -15,48 +16,19 @@ import java.util.SortedMap;
 import java.util.concurrent.ConcurrentSkipListMap;
 
 public class DistributedDao implements Dao<MemorySegment, Entry<MemorySegment>> {
-    private static int MULTIPLICATION_FACTOR = 3;
-    private final SortedMap<byte[], Dao<MemorySegment, Entry<MemorySegment>>> nodeRing = new ConcurrentSkipListMap<>(
-            (b1, b2) -> {
-                for (int i = 0; i < Math.min(b1.length, b2.length); i++) {
-                    if (b1[i] == b2[i]) continue;
-                    else if (b1[i] > b2[i]) {
-                        return 1;
-                    } else return -1;
-                }
-                if (b1.length == b2.length) {
-                    return 0;
-                }
-                if (b1.length > b2.length) return 1;
-                else return -1;
-            }
-    );
-
-    private final MessageDigest messageDigest;
-
-    public DistributedDao() {
-        try {
-            messageDigest = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            throw new MissingRequiredHashingAlgorytmException(e);
-        }
-    }
-
-    private static class MissingRequiredHashingAlgorytmException extends RuntimeException {
-        public MissingRequiredHashingAlgorytmException(Exception e) {
-            super(e);
-        }
-    }
+    private static int MULTIPLICATION_FACTOR = 12;
+    private final SortedMap<Integer, Dao<MemorySegment, Entry<MemorySegment>>> nodeRing = new ConcurrentSkipListMap<>();
 
     public void addNode(Dao<MemorySegment, Entry<MemorySegment>> daoNode, String token) {
         for (int i = 0; i < MULTIPLICATION_FACTOR; i++) {
-            nodeRing.put(messageDigest.digest((token + i).getBytes(StandardCharsets.UTF_8)), daoNode);
+
+            nodeRing.put(Hash.murmur3((token + i)), daoNode);
         }
     }
 
-    private Dao<MemorySegment, Entry<MemorySegment>> selectNode(byte[] key) {
-        Map.Entry<byte[], Dao<MemorySegment, Entry<MemorySegment>>> nodeEntry =
-                this.nodeRing.tailMap(messageDigest.digest(key)).firstEntry();
+    private Dao<MemorySegment, Entry<MemorySegment>> selectNode(String key) {
+        Map.Entry<Integer, Dao<MemorySegment, Entry<MemorySegment>>> nodeEntry =
+                this.nodeRing.tailMap(Hash.murmur3(key)).firstEntry();
         if (nodeEntry == null) {
             return this.nodeRing.firstEntry().getValue();
         }
@@ -71,13 +43,13 @@ public class DistributedDao implements Dao<MemorySegment, Entry<MemorySegment>> 
 
     @Override
     public Entry<MemorySegment> get(MemorySegment key) {
-        Dao<MemorySegment, Entry<MemorySegment>> dao = selectNode(key.toArray(ValueLayout.JAVA_BYTE));
+        Dao<MemorySegment, Entry<MemorySegment>> dao = selectNode(new String(key.toArray(ValueLayout.JAVA_BYTE), StandardCharsets.UTF_8));
         return dao.get(key);
     }
 
     @Override
     public void upsert(Entry<MemorySegment> entry) {
-        Dao<MemorySegment, Entry<MemorySegment>> dao = selectNode(entry.key().toArray(ValueLayout.JAVA_BYTE));
+        Dao<MemorySegment, Entry<MemorySegment>> dao = selectNode(new String(entry.key().toArray(ValueLayout.JAVA_BYTE), StandardCharsets.UTF_8));
         dao.upsert(entry);
     }
 

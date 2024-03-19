bb9693a3bc9b3d377ce4a09e32f854266514fce1
kislovda <kislovda@mos-team.ru>
2024-03-19 19:21:15 +0300

    Linter fix

--- src/main/java/ru/vk/itmo/test/kislovdanil/service/sharding/RandevouzSharder.java
+++ src/main/java/ru/vk/itmo/test/kislovdanil/service/sharding/RandevouzSharder.java
@@ -3,7 +3,6 @@ package ru.vk.itmo.test.kislovdanil.service.sharding;
 import ru.vk.itmo.test.kislovdanil.service.InvalidTopologyError;
 
 import java.net.http.HttpClient;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,7 +16,7 @@ public class RandevouzSharder extends BaseSharder {
         super(client);
         this.urls = urls;
         final Random random = new Random(urls.getFirst().hashCode());
-        for (String url: urls) {
+        for (String url : urls) {
             urlsHash.put(url, random.nextInt(100000));
         }
     }
@@ -31,7 +30,7 @@ public class RandevouzSharder extends BaseSharder {
     public String defineRequestProxyUrl(String entityKey) {
         int maxHash = Integer.MIN_VALUE;
         String maxHashUrl = null;
-        for (String url: urls) {
+        for (String url : urls) {
             int hash = getHash(url, entityKey);
             if (hash >= maxHash) {
                 maxHash = hash;

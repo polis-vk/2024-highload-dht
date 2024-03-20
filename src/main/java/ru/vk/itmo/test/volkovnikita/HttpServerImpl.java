a134cb4652b8e09df9cdb01f4b1e9c9baa75e6f1
ravenhub <nikita.volkov0505@mail.ru>
2024-03-20 14:30:06 +0300

    refactoring

--- src/main/java/ru/vk/itmo/test/volkovnikita/HttpServerImpl.java
+++ src/main/java/ru/vk/itmo/test/volkovnikita/HttpServerImpl.java
@@ -1,14 +1,6 @@
 package ru.vk.itmo.test.volkovnikita;
 
-import one.nio.http.HttpException;
-import one.nio.http.HttpServer;
-import one.nio.http.HttpServerConfig;
-import one.nio.http.HttpSession;
-import one.nio.http.Param;
-import one.nio.http.Path;
-import one.nio.http.Request;
-import one.nio.http.RequestMethod;
-import one.nio.http.Response;
+import one.nio.http.*;
 import one.nio.server.AcceptorConfig;
 import one.nio.util.Hash;
 import org.slf4j.Logger;
@@ -34,12 +26,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executor;
-import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class HttpServerImpl extends HttpServer {

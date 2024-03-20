3b56dac541a8e41a7aa033b3b456d4baa52e360c
vitekkor <viktor.korotkikh@gmail.com>
2024-03-20 20:54:36 +0300

    stage-3: use murmur hashing

--- src/main/java/ru/vk/itmo/test/viktorkorotkikh/ConsistentHashingManager.java
+++ src/main/java/ru/vk/itmo/test/viktorkorotkikh/ConsistentHashingManager.java
@@ -1,10 +1,8 @@
 package ru.vk.itmo.test.viktorkorotkikh;
 
-import ru.vk.itmo.test.viktorkorotkikh.exception.NotSupportedAlgorithmException;
+import one.nio.util.Hash;
 
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.NavigableMap;
 import java.util.SortedMap;
@@ -15,31 +13,21 @@ public class ConsistentHashingManager {
 
     public ConsistentHashingManager(int numberOfVNodes, List<String> clusterUrls) {
         hashRing = new TreeMap<>();
-        MessageDigest messageDigestInstance = getMessageDigestInstance();
         for (String clusterUrl : clusterUrls) {
             for (int j = 0; j < numberOfVNodes; j++) {
                 final byte[] input = (clusterUrl + j).getBytes(StandardCharsets.UTF_8);
-                final byte[] hash = messageDigestInstance.digest(input);
-                hashRing.put(hashToInt(hash), clusterUrl);
+                hashRing.put(bytesToHash(input), clusterUrl);
             }
         }
     }
 
     public String getServerByKey(final byte[] key) {
-        final int keyHash = hashToInt(getMessageDigestInstance().digest(key));
+        final int keyHash = bytesToHash(key);
         SortedMap<Integer, String> tailMap = hashRing.tailMap(keyHash);
         return (tailMap.isEmpty() ? hashRing.firstEntry() : tailMap.firstEntry()).getValue();
     }
 
-    private static MessageDigest getMessageDigestInstance() {
-        try {
-            return MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            throw new NotSupportedAlgorithmException("SHA-256 not supported", e);
-        }
-    }
-
-    private static int hashToInt(byte[] bytes) {
-        return bytes[0] << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
+    private static int bytesToHash(byte[] bytes) {
+        return Hash.murmur3(bytes, 0, bytes.length);
     }
 }

3c5aba8213114f9fa8819a9db114889190b9afd8
MaxTTG <m-tarazanov@list.ru>
2024-03-27 02:35:25 +0300

    stage4 started

---
+++ src/main/java/ru/vk/itmo/test/tarazanovmaxim/MyFactory.java
@@ -0,0 +1,14 @@
+package ru.vk.itmo.test.tarazanovmaxim;
+
+import ru.vk.itmo.Service;
+import ru.vk.itmo.ServiceConfig;
+import ru.vk.itmo.test.ServiceFactory;
+
+@ServiceFactory(stage = 4)
+public class MyFactory implements ServiceFactory.Factory {
+
+    @Override
+    public Service create(ServiceConfig config) {
+        return new MyService(config);
+    }
+}

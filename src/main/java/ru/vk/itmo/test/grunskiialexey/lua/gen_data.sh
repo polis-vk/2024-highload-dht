788f2edc929a83aac40460d6dab4efb0e83ba589
Alexey Grunskii <robochat@mail.ru>
2024-04-01 21:21:56 +0300

    Save images and paste in inside my github repository

--- src/main/java/ru/vk/itmo/test/grunskiialexey/lua/gen_data.sh
+++ src/main/java/ru/vk/itmo/test/grunskiialexey/lua/gen_data.sh
@@ -3,7 +3,7 @@ do
   file="$1_d$2_c$3_t$4_r$5_$i"
   full="full_$file.txt"
   hist="hist_$file.txt"
-  profile="profile_e$7_$file.html"
+  profile="profile_$7_$file.html"
   wrk -s "$1.lua" -d $2 -c $3 -t $4 -R $5 -L http://localhost:8081 > $full &
   asprof -d $2 -f $profile -e $7 DaoServer
   cp $full "./full/$full"

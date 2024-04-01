e305f044fb6319f172d4a57e9ac8e6131fab1cce
Viktor <36002722+vitekkor@users.noreply.github.com>
2024-04-01 12:03:59 +0300

    Коротких Виктор / ИТМО DWS / Stage 3 (#114)
    * stage-3: pre-start all threads
    
    * stage-3: implement consistent hashing
    
    * stage-3: remove debug tmpDirs; remove unused imports
    
    * stage-3: fix comments
    
    * stage-3: close resources
    
    * stage-3: add local dev arg parsing
    
    * stage-3: add PUT results
    
    * stage-3: use murmur hashing
    
    * stage-3: minors
    
    * stage-3: trigger build
    
    * stage-3: delete report
    
    * stage-3: add put report
    
    * stage-3: add get results
    
    * stage-3: add report

--- src/main/java/ru/vk/itmo/test/viktorkorotkikh/scripts/stage-3-GET-key-miss.lua
+++ src/main/java/ru/vk/itmo/test/viktorkorotkikh/scripts/stage-3-GET-key-miss.lua
@@ -1,15 +1,15 @@
 ---
 --- Created by vitekkor.
---- DateTime: 17.02.2024 19:06
+--- DateTime: 17.03.2024 21:16
 ---
 
 host = "localhost"
 port = 8080
 
-math.randomseed(os.clock()) --- 1709820295
+math.randomseed(os.clock())
 
 request = function()
-    rnd = math.random(750000, 2250000)
+    rnd = math.random(2000000, 3500000)
     path = "/v0/entity?id=key" .. rnd
     method = "GET"
     return wrk.format(method, path)

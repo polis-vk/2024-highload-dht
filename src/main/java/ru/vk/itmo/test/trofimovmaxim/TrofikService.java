08cf8f3aa5f6deec846eb99a413133bd6904a0a2
Trofimov Maksim <maks.trofimov.0303@mail.ru>
2024-04-03 22:06:27 +0300

    add reference for stage-4

--- src/main/java/ru/vk/itmo/test/trofimovmaxim/TrofikService.java
+++ src/main/java/ru/vk/itmo/test/trofimovmaxim/TrofikService.java
@@ -2,33 +2,68 @@ package ru.vk.itmo.test.trofimovmaxim;
 
 import ru.vk.itmo.Service;
 import ru.vk.itmo.ServiceConfig;
+import ru.vk.itmo.dao.Config;
 import ru.vk.itmo.test.ServiceFactory;
+import ru.vk.itmo.test.trofimovmaxim.dao.ReferenceDao;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class TrofikService implements Service {
-    private TrofikServer server;
+
+    private static final long FLUSHING_THRESHOLD_BYTES = 1024 * 1024;
+
     private final ServiceConfig config;
 
+    private ReferenceDao dao;
+    private TrofikServer server;
+    private boolean stopped;
     public TrofikService(ServiceConfig config) {
         this.config = config;
     }
 
     @Override
-    public CompletableFuture<Void> start() throws IOException {
-        server = new TrofikServer(config);
+    public synchronized CompletableFuture<Void> start() throws IOException {
+        dao = new ReferenceDao(new Config(config.workingDir(), FLUSHING_THRESHOLD_BYTES));
+        server = new TrofikServer(config, dao);
         server.start();
+        stopped = false;
         return CompletableFuture.completedFuture(null);
     }
 
     @Override
-    public CompletableFuture<Void> stop() {
-        server.stop();
+    public synchronized CompletableFuture<Void> stop() throws IOException {
+        if (stopped) {
+            return CompletableFuture.completedFuture(null);
+        }
+        try {
+            server.stop();
+
+        } finally {
+            dao.close();
+        }
+        stopped = true;
         return CompletableFuture.completedFuture(null);
     }
 
-    @ServiceFactory(stage = 1)
+    public static void shutdownAndAwaitTermination(ExecutorService pool) {
+        pool.shutdown();
+        try {
+            if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                pool.shutdownNow();
+                if (!pool.awaitTermination(60, TimeUnit.SECONDS)) {
+                    System.err.println("Pool did not terminate");
+                }
+            }
+        } catch (InterruptedException ex) {
+            pool.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    @ServiceFactory(stage = 4)
     public static class Factory implements ServiceFactory.Factory {
 
         @Override

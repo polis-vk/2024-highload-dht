cb69a8738581673ba12a33139f36d36c42bb8f3c
IsqK <plabolchar23@gmail.com>
2024-04-03 20:28:39 +0300

    fixes

--- src/main/java/ru/vk/itmo/test/kovalevigor/server/strategy/decorators/ServerExecutorStrategyDecorator.java
+++ src/main/java/ru/vk/itmo/test/kovalevigor/server/strategy/decorators/ServerExecutorStrategyDecorator.java
@@ -24,6 +24,8 @@ import static ru.vk.itmo.test.kovalevigor.server.util.ServerUtil.shutdownAndAwai
 public class ServerExecutorStrategyDecorator extends ServerStrategyDecorator implements RejectedExecutionHandler {
     private final ThreadPoolExecutor mainExecutor;
     private final Map<Thread, ThreadPoolExecutor> executors;
+    private final long keepAliveTime;
+    private static final int SUB_EXECUTORS_POOL_SIZE = 1;
 
     public ServerExecutorStrategyDecorator(
             ServerStrategy serverStrategy,
@@ -31,17 +33,18 @@ public class ServerExecutorStrategyDecorator extends ServerStrategyDecorator imp
             long keepAliveTime, int queueCapacity
     ) {
         super(serverStrategy);
-        executors = new HashMap<>();
+        this.executors = new HashMap<>();
+        this.keepAliveTime = keepAliveTime;
         int realCorePoolSize = corePoolSize == 1 ? 1 : corePoolSize / 2;
-        mainExecutor = new ThreadPoolExecutor(
+        this.mainExecutor = new ThreadPoolExecutor(
                 realCorePoolSize,
                 maximumPoolSize - realCorePoolSize,
-                keepAliveTime,
+                this.keepAliveTime,
                 TimeUnit.SECONDS,
-                new LinkedBlockingQueue<>(queueCapacity * 10),
+                new LinkedBlockingQueue<>(queueCapacity * realCorePoolSize),
                 new CustomPolicy()
         );
-        mainExecutor.prestartAllCoreThreads();
+        this.mainExecutor.prestartAllCoreThreads();
     }
 
     private class CustomPolicy implements RejectedExecutionHandler {
@@ -80,12 +83,12 @@ public class ServerExecutorStrategyDecorator extends ServerStrategyDecorator imp
         ThreadPoolExecutor[] threadPoolExecutors = new ThreadPoolExecutor[mainExecutor.getCorePoolSize()];
         for (int i = 0; i < threadPoolExecutors.length; i++) {
             threadPoolExecutors[i] = new ThreadPoolExecutor(
-                    1,
-                    1,
-                    100,
+                    SUB_EXECUTORS_POOL_SIZE,
+                    SUB_EXECUTORS_POOL_SIZE,
+                    this.keepAliveTime,
                     TimeUnit.SECONDS,
                     new LinkedBlockingQueue<>(
-                            mainExecutor.getQueue().remainingCapacity() / 10
+                            mainExecutor.getQueue().remainingCapacity() / mainExecutor.getCorePoolSize()
                     ),
                     this
             );

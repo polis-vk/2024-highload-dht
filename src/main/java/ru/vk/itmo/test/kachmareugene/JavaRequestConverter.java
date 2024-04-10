31b263031460fe187a2452fd6e1a6d67ee79ce22
Kachmar Eugene <kachmar521@mail.ru>
2024-04-10 23:08:33 +0300

    codeclimate mmm so sweet!

--- src/main/java/ru/vk/itmo/test/kachmareugene/JavaRequestConverter.java
+++ src/main/java/ru/vk/itmo/test/kachmareugene/JavaRequestConverter.java
@@ -9,7 +9,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Map;
 
-public class JavaRequestConverter {
+public final class JavaRequestConverter {
     private static final Map<Integer, String> intToStringCode =
             Map.of(
             202, Response.ACCEPTED,

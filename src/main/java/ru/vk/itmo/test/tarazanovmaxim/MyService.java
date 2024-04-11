ae5522f8d84834ee580edf065500d19d45f31977
Таразанов Максим Сергеевич <m-tarazanov@list.ru>
2024-04-11 12:31:45 +0300

    stage5 without report + codeclimate

--- src/main/java/ru/vk/itmo/test/tarazanovmaxim/MyService.java
+++ src/main/java/ru/vk/itmo/test/tarazanovmaxim/MyService.java
@@ -5,13 +5,14 @@ import ru.vk.itmo.ServiceConfig;
 
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MyService implements Service {
     private MyServer server;
 
     private final ServiceConfig config;
 
-    private boolean stopped;
+    private AtomicBoolean stopped;
 
     public MyService(ServiceConfig config) {
         this.config = config;
@@ -20,20 +21,19 @@ public class MyService implements Service {
     @Override
     public CompletableFuture<Void> start() throws IOException {
         server = new MyServer(config);
-        stopped = false;
+        stopped = new AtomicBoolean(false);
         server.start();
         return CompletableFuture.completedFuture(null);
     }
 
     @Override
     public CompletableFuture<Void> stop() throws IOException {
-        if (stopped) {
+        if (stopped.compareAndSet(false, true)) {
+            server.close();
+            server.stop();
             return CompletableFuture.completedFuture(null);
         }
 
-        stopped = true;
-        server.close();
-        server.stop();
         return CompletableFuture.completedFuture(null);
     }
 }

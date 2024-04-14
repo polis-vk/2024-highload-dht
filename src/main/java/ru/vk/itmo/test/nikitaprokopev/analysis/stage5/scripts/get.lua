b25864404396bbdcc6bb4a9b418690369df5554e
Nikita Prokopev <nikpro200125@gmail.com>
2024-04-14 20:27:13 +0300

    add wrk + profiling

--- src/main/java/ru/vk/itmo/test/nikitaprokopev/analysis/stage5/scripts/get.lua
+++ src/main/java/ru/vk/itmo/test/nikitaprokopev/analysis/stage5/scripts/get.lua
@@ -3,5 +3,5 @@ function request()
     headers["Host"] = "localhost:8080"
     local id = math.random(1, 10000)
     local key = "key" .. id
-    return wrk.format("GET", "/v0/entity?id=" .. key .. "&ack=3&from=3", headers)
+    return wrk.format("GET", "/v0/entity?id=" .. key .. "&ack=1&from=3", headers)
 end

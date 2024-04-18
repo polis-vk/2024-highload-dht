2dc32a4efa521536287c8b975491c6e7e361507a
pashchenko8 <53857982+pashchenko8@users.noreply.github.com>
2024-04-18 12:24:54 +0300

    reference s5

---
+++ src/main/java/ru/vk/itmo/test/reference/HandleResult.java
@@ -0,0 +1,14 @@
+package ru.vk.itmo.test.reference;
+
+public record HandleResult(int status, byte[] data, long timestamp) {
+
+    public HandleResult(int status, byte[] data, long timestamp) {
+        this.status = status;
+        this.data = data;
+        this.timestamp = timestamp;
+    }
+
+    public HandleResult(int status, byte[] data) {
+        this(status, data, 0);
+    }
+}

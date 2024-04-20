08bff6f3e81533d903a80d328d9312e337e79ed7
Vladislav Bandurin <93590005+vbandurin7@users.noreply.github.com>
2024-04-20 18:56:25 +0300

    Бандурин Владислав, ИТМО ФИТиП М3332, Stage 4 (#152)
    * stage 1 done
    
    * Uncommented -werror
    
    * Codeclimate satisfy
    
    * Codeclimate satisfy
    
    * Changed ThresholdBytes back
    
    * (WIP) some changes for async
    
    * Review improvements
    
    * Codeclimate
    
    * codeclimate
    
    * Codeclimate
    
    * Codeclimate
    
    * Codeclimate
    
    * Fixes and new features
    
    * Completed stage-2
    
    * HW3 in a rush :)
    
    * Codeclimate
    
    * improved report
    
    * Added line :)
    
    * Codeclimate
    
    * stage-3 completed
    
    * completed stage-3
    
    * completed stage-3
    
    * completed stage-3
    
    * stage-4 code added
    
    * stage-4 code added
    
    * codeclimate
    
    * codeclimate
    
    * codeclimate
    
    * codeclimate
    
    * check flaky test
    
    * check flaky test
    
    * Code refactoring + report
    
    * First review fixes
    
    * Finished fixes
    
    * Update report.md
    
    * Update report.md
    
    * Minor fixes
    
    * Codeclimate
    
    ---------
    
    Co-authored-by: Vadim Tsesko <incubos@users.noreply.github.com>
    Co-authored-by: Фёдор Надуткин <59574675+FedosOnGIT@users.noreply.github.com>

--- src/main/java/ru/vk/itmo/test/bandurinvladislav/util/Constants.java
+++ src/main/java/ru/vk/itmo/test/bandurinvladislav/util/Constants.java
@@ -2,12 +2,19 @@ package ru.vk.itmo.test.bandurinvladislav.util;
 
 public final class Constants {
     public static final String ENDPOINT = "/v0/entity";
+    public static final String HEADER_TIMESTAMP = "X-Timestamp: ";
+    public static final String HEADER_SENDER = "X-Sender: ";
     public static final String PARAMETER_ID = "id=";
+    public static final String PARAMETER_ACK = "ack=";
+    public static final String PARAMETER_FROM = "from=";
     public static final String TOO_MANY_REQUESTS = "429 too many requests";
+    public static final String NOT_ENOUGH_REPLICAS = "504 Not Enough Replicas";
 
+    public static final int CLIENT_RESPONSE_TIMEOUT_MILLIS = 100;
     public static final int THREADS = Runtime.getRuntime().availableProcessors();
+    public static final int THREAD_KEEP_ALIVE_TIME = 60;
     public static final long TASK_DEADLINE_MILLIS = 500;
-    public static final int FLUSH_THRESHOLD_BYTES = 10 * 1024 * 1024;
+    public static final int FLUSH_THRESHOLD_BYTES = 3 * 1024 * 1024;
     public static final int QUEUE_SIZE = 128;
 
     private Constants() {

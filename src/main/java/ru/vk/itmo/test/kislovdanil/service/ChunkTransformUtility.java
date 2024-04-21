7cd7b1919afee22f8c8607d234ee220b758c477b
kislovda <kislovda@mos-team.ru>
2024-04-21 18:54:40 +0300

    Fix lint

--- src/main/java/ru/vk/itmo/test/kislovdanil/service/ChunkTransformUtility.java
+++ src/main/java/ru/vk/itmo/test/kislovdanil/service/ChunkTransformUtility.java
@@ -19,11 +19,12 @@ public class ChunkTransformUtility {
     public static byte[] makeContent(Entry<MemorySegment> entry) {
         int entrySize = (int) (entry.key().byteSize() + entry.value().byteSize()) + KEY_VALUE_SEPARATOR.length();
         String entrySizeHex = Long.toHexString(entrySize);
-        String content = entrySizeHex + CHUNK_SEPARATOR +
-                new String(entry.key().toArray(ValueLayout.JAVA_BYTE), StandardCharsets.UTF_8) +
-                KEY_VALUE_SEPARATOR +
-                new String(entry.value().toArray(ValueLayout.JAVA_BYTE), StandardCharsets.UTF_8) +
-                CHUNK_SEPARATOR;
+        String content = entrySizeHex
+                + CHUNK_SEPARATOR
+                + new String(entry.key().toArray(ValueLayout.JAVA_BYTE), StandardCharsets.UTF_8)
+                + KEY_VALUE_SEPARATOR
+                + new String(entry.value().toArray(ValueLayout.JAVA_BYTE), StandardCharsets.UTF_8)
+                + CHUNK_SEPARATOR;
         return content.getBytes(StandardCharsets.UTF_8);
     }
 }

5ca395338a2e45b4afe438e6f48b60dc3d55e5cb
Vladislav Bandurin <93590005+vbandurin7@users.noreply.github.com>
2024-04-22 14:51:56 +0300

    Codeclimate

--- src/main/java/ru/vk/itmo/test/bandurinvladislav/Server.java
+++ src/main/java/ru/vk/itmo/test/bandurinvladislav/Server.java
@@ -31,7 +31,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.CompletableFuture;
-
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.TimeUnit;
 

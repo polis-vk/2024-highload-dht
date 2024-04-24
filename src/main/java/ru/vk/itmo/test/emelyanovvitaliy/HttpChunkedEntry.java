493e151861767fd8858778608865dcd267881da9
vitaliy.emelyanov <vitaliy.emelyanov@yadro.com>
2024-04-25 02:48:27 +0300

    6th stage report and fixes
    Signed-off-by: vitaliy.emelyanov <vitaliy.emelyanov@yadro.com>

--- src/main/java/ru/vk/itmo/test/emelyanovvitaliy/HttpChunkedEntry.java
+++ src/main/java/ru/vk/itmo/test/emelyanovvitaliy/HttpChunkedEntry.java
@@ -21,26 +21,23 @@ public class HttpChunkedEntry extends Session.QueueItem {
     public int write(Socket socket) throws IOException {
         byte[] key = getBytes(entry.key());
         byte[] value = getBytes(entry.value());
-        String length = Long.toHexString(key.length + value.length + SEPARATOR.length);
-        int written = 0;
-        written += writeWithBreak(socket, length.getBytes(StandardCharsets.UTF_8));
-        written += writeWithBreak(socket, key);
-        written += writeWithBreak(socket, value);
-        return written;
+        byte[] length = Long.toHexString(key.length + value.length + SEPARATOR.length)
+                .getBytes(StandardCharsets.UTF_8);
+        int offset = 0;
+        byte[] output = new byte[length.length + key.length + value.length + 3 * SEPARATOR.length];
+        offset += copy(length, output, offset);
+        offset += copy(SEPARATOR, output, offset);
+        offset += copy(key, output, offset);
+        offset += copy(SEPARATOR, output, offset);
+        offset += copy(value, output, offset);
+        offset += copy(SEPARATOR, output, offset);
+        socket.write(output, 0, output.length);
+        return offset;
     }
 
-    private static int br(Socket socket) throws IOException {
-        socket.write(SEPARATOR, 0, SEPARATOR.length);
-        return SEPARATOR.length;
-    }
-
-    private static int write(Socket socket, byte[] bytes) throws IOException {
-        socket.write(bytes, 0, bytes.length);
-        return bytes.length;
-    }
-
-    private static int writeWithBreak(Socket socket, byte[] bytes) throws IOException {
-        return write(socket, bytes) + br(socket);
+    private static int copy(byte[] from, byte[] to, int destOffset) {
+        System.arraycopy(from, 0, to, destOffset, from.length);
+        return from.length;
     }
 
     private static byte[] getBytes(MemorySegment segment) {

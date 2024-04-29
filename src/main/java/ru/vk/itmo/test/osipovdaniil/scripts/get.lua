36669a4c65862b92631f2f5f90b0c23234d95b7e
sbread <bestoffirst@yandex.ru>
2024-04-30 02:36:23 +0300

    report5

--- src/main/java/ru/vk/itmo/test/osipovdaniil/scripts/get.lua
+++ src/main/java/ru/vk/itmo/test/osipovdaniil/scripts/get.lua
@@ -3,8 +3,8 @@ id = 0
 function request()
     curId = id
     id = id + 1
-    path = "/v0/entity?id=key" .. curId
+    path = "/v0/entity?id=key" .. curId .. "&ack=2&from=3"
     headers = {}
-    headers["Host"] = "localhost:8080"
+    headers["Host"] = "localhost:808" .. string.char(math.random(1, 4))
     return wrk.format("GET", path, headers)
 end
--- src/main/java/ru/vk/itmo/test/osipovdaniil/scripts/put.lua
+++ src/main/java/ru/vk/itmo/test/osipovdaniil/scripts/put.lua
@@ -3,9 +3,9 @@ id = 0
 function request()
     curId = id
     id = id + 1
-    path = "/v0/entity?id=key" .. curId
+    path = "/v0/entity?id=key" .. curId .. "&ack=2&from=3"
     headers = {}
-    headers["Host"] = "localhost:8080"
+    headers["Host"] = "localhost:808" .. string.char(math.random(1, 4))
     value = "value" .. curId
     return wrk.format("PUT", path, headers, value)
 end

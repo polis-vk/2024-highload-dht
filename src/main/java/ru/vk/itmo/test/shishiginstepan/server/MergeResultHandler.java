8046881bcbeba01c66273636acc6f0be98e4eda6
Stepan <sshishigin190101@gmail.com>
2024-05-11 11:49:06 +0300

    HW 5 Шишигин Степан DWS ИТМО (#197)
    * dao init
    
    * service init
    
    * server init
    
    * lua random read write delete script
    
    * codestyle fixes
    
    * codestyle fixes
    
    * codestyle fixes
    
    * codestyle fixes
    
    * codestyle fixes
    
    * codestyle fixes
    
    * codestyle fixes
    
    * some dao fixes
    
    * lua script fixes
    
    * lua script fixes
    
    * report with artifacts
    
    * Update README.md (#42)
    
    (cherry picked from commit ea6b417682628d3431eade352a5d9ecdadcb42b4)
    
    * mem storage limit increased
    
    * some script refactoring
    
    * first straightforward solution (executor.execute in overriden handleRequest)
    
    * small refactoring and thread numbers shuffle
    
    * Nonnul annotation missed
    
    * logging level in dao
    
    * fix checkstyle?
    
    * fix checkstyle?
    
    * checkstyle
    
    * checkstyle
    
    * two-level exception handling
    if handled succesfully - send errorcode
    if handling failed - close connection
    
    * code climate fix attempt
    
    * code climate fix attempt 2
    
    * code climate победил
    
    * code climate fix
    
    * code climate fix reverted
    
    * отчёт
    
    * фикс количества потоков в пуле
    
    * SMALL fix, relative path in REPORT
    
    * server correct resource management
    
    * dao correct resource management
    
    * remote dao connection impl
    
    * dao manager impl
    
    * dao manager substitute for server instead of just local dao
    
    * codestyle fix
    
    * codestyle fix
    
    * codestyle fix
    
    * codestyle fix
    
    * change hashing from java message digest to one nio murmur3 implementation
    
    * v-node tuning and import cleanup + logical mistake
    
    * начала отчета, пока что без профилей
    
    * codestyle fix
    
    * остатки отчета
    
    * SOLUTION
    
    * codestyle
    
    * codestyle
    
    * codestyle
    
    * codestyle
    
    * отчет
    
    * фиксы по комментам
    
    * фиксы по комментам
    
    * broken totally
    
    * still broken
    
    * more broken than ever
    
    * probably fixed
    
    * actually fixed!!!
    
    * codestyle
    
    * codestyle
    
    * oopsie
    
    * code climate
    
    * code climate
    
    * code climate
    
    * code climate
    
    * code climate
    
    * code climate
    
    * maybe fix
    
    * maybe fix
    
    * one more fix (code was running in selectors)
    
    * code climate
    
    * report
    
    ---------
    
    Co-authored-by: Степан <s.l.shishigin@neovoxtech.ru>
    Co-authored-by: Vadim Tsesko <incubos@users.noreply.github.com>

---
+++ src/main/java/ru/vk/itmo/test/shishiginstepan/server/MergeResultHandler.java
@@ -0,0 +1,78 @@
+package ru.vk.itmo.test.shishiginstepan.server;
+
+import one.nio.http.HttpSession;
+import one.nio.http.Response;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+class MergeResultHandler {
+    private final Logger logger = Logger.getLogger("lsm-db-server");
+    private final HttpSession session;
+    private final AtomicInteger totalResponseCounter;
+    private final AtomicInteger successCounter;
+
+    private final ConcurrentSkipListSet<ResponseWrapper> results;
+
+    private final AtomicBoolean closed = new AtomicBoolean(false);
+
+    MergeResultHandler(Integer ack, Integer from, HttpSession session) {
+        this.session = session;
+        this.totalResponseCounter = new AtomicInteger(from);
+        this.successCounter = new AtomicInteger(ack);
+        this.results = new ConcurrentSkipListSet<>((r1, r2) -> Long.compare(r2.timestamp(), r1.timestamp()));
+    }
+
+    public synchronized void add(ResponseWrapper response) {
+        if (closed.get()) {
+            return;
+        }
+        int total = this.totalResponseCounter.decrementAndGet();
+        int success;
+        if ((response.status() == HttpURLConnection.HTTP_OK
+                || response.status() == HttpURLConnection.HTTP_CREATED
+                || response.status() == HttpURLConnection.HTTP_ACCEPTED
+                || response.status() == HttpURLConnection.HTTP_NOT_FOUND)) {
+            success = successCounter.decrementAndGet();
+            results.add(response);
+            if (success == 0) {
+                closed.set(true);
+                sendResponse();
+                return;
+            }
+        }
+        if (total == 0) {
+            closed.set(true);
+            sendError();
+        }
+
+    }
+
+    private void sendResponse() {
+        try {
+            ResponseWrapper response = results.getFirst();
+            session.sendResponse(
+                    new Response(
+                            String.valueOf(response.status()),
+                            response.data()
+                    )
+            );
+        } catch (IOException e) {
+            logger.error(e);
+            session.close();
+        }
+    }
+
+    private void sendError() {
+        try {
+            session.sendResponse(new Response(Response.GATEWAY_TIMEOUT, Response.EMPTY));
+        } catch (IOException e) {
+            logger.error(e);
+            session.close();
+        }
+    }
+}
